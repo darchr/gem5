4350b1448ba4df85b349f3f3a0b1ca337c7f88e8
Jason Lowe-Power <jason@lowepower.com>
2017-06-19 16:30:51 -0500

    cpu: Add a *very* simple CPU model
    This CPU model currently doesn't work at all, but it does build.
    
    The idea is to make a simple CPU model so I can understand how the CPU
    models in gem5 work. This is part of the Learning gem5 book. See
    learning.gem5.org/ for more information.
    
    Change-Id: I0d33dd7c8174be8ab2d7f359d2baf5e9ab2740b3
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ src/cpu/learning_simple_cpu/cpu.cc
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2017 Jason Lowe-Power
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Jason Lowe-Power
+ */
+
+#include "cpu/learning_simple_cpu/cpu.hh"
+
+#include "debug/LearningSimpleCPU.hh"
+
+LearningSimpleCPU::LearningSimpleCPU(LearningSimpleCPUParams *params) :
+    BaseCPU(params),
+    port(name()+".port", this),
+    memOutstanding(false)
+{
+}
+
+void
+LearningSimpleCPU::wakeup(ThreadID tid)
+{
+    assert(tid == 0); // This CPU doesn't support more than one thread!
+
+    // Not sure what to do here.
+
+    //  if (threadInfo[tid]->thread->status() == ThreadContext::Suspended) {
+    //      DPRINTF(SimpleCPU,"[tid:%d] Suspended Processor awoke\n", tid);
+    //      threadInfo[tid]->thread->activate();
+    //  }
+}
+
+void
+LearningSimpleCPU::CPUPort::sendPacket(PacketPtr pkt)
+{
+    // Note: This flow control is very simple since the memobj is blocking.
+
+    panic_if(blockedPacket != nullptr, "Should never try to send if blocked!");
+
+    // If we can't send the packet across the port, store it for later.
+    if (!sendTimingReq(pkt)) {
+        blockedPacket = pkt;
+    }
+}
+
+bool
+LearningSimpleCPU::CPUPort::recvTimingResp(PacketPtr pkt)
+{
+    // Just forward to the memobj.
+    return owner->handleResponse(pkt);
+}
+
+void
+LearningSimpleCPU::CPUPort::recvReqRetry()
+{
+    // We should have a blocked packet if this function is called.
+    assert(blockedPacket != nullptr);
+
+    // Grab the blocked packet.
+    PacketPtr pkt = blockedPacket;
+    blockedPacket = nullptr;
+
+    // Try to resend it. It's possible that it fails again.
+    sendPacket(pkt);
+}
+
+bool
+LearningSimpleCPU::handleResponse(PacketPtr pkt)
+{
+    assert(memOutstanding);
+    DPRINTF(LearningSimpleCPU, "Got response for addr %#x\n", pkt->getAddr());
+
+    // The packet is now done. We're about to put it in the port, no need for
+    // this object to continue to stall.
+    // We need to free the resource before sending the packet in case the CPU
+    // tries to send another request immediately (e.g., in the same callchain).
+    memOutstanding = false;
+
+    // Do something?
+
+    return true;
+}
+
+LearningSimpleCPU*
+LearningSimpleCPUParams::create()
+{
+    return new LearningSimpleCPU(this);
+}
