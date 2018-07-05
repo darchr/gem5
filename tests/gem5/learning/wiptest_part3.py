74b8df112fa271034afd9fdacf79716e0f94d04c
Jason Lowe-Power <jason@lowepower.com>
2018-07-05 16:46:19 -0700

    tests: Add tests for learning gem5
    Change-Id: Ic9fd58bc0ec497898d4781de344426ffa5bd1286
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ tests/gem5/learning/wiptest_part3.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2018 The Regents of the University of California
+# All Rights Reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Jason Lowe-Power
+
+'''
+Test file for the scripts from Learning gem5 Part 1
+'''
+
+from testlib import *
+
+ref_path = joinpath(getcwd(), 'ref', 'simout-hello')
+
+verifiers = (
+        verifier.MatchStdoutNoPerf(ref_path),
+)
+
+gem5_verify_config(
+        name='learning_gem5_part3_simple',
+        verifiers=verifiers,
+        config=joinpath(config.base_dir, 'configs', 'learning_gem5',
+                        'part3', 'simple_ruby.py'),
+        config_args=[],
+        valid_isas=(constants.x86_tag, constants.arm_tag),
+)
