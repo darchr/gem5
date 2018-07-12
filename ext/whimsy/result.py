176fc4ee20fceba5dea11dbb905ec4a702bc2948
Sean Wilson <spwilson2@wisc.edu>
2018-07-12 12:52:55 -0500

    Ignore no stdout/err file found on result.xml write

--- ext/whimsy/result.py
+++ ext/whimsy/result.py
@@ -68,7 +68,7 @@ class InternalTestResult(object, _CommonMetadataMixin):
     def __init__(self, obj, suite, directory):
         self._metadata = obj.metadata
         self.suite = suite
-    
+
         self.stderr = os.path.join(
             InternalSavedResults.output_path(self.uid, suite.uid),
             'stderr'
@@ -84,21 +84,21 @@ class InternalSuiteResult(object, _CommonMetadataMixin):
         self._metadata = obj.metadata
         self.directory = directory
         self._wrap_tests(obj)
-    
+
     def _wrap_tests(self, obj):
-        self._tests = [InternalTestResult(test, self, self.directory) 
+        self._tests = [InternalTestResult(test, self, self.directory)
                        for test in obj]
         self._tests_index = _create_uid_index(self._tests)
-    
+
     def get_test(self, uid):
         return self._tests_index[uid]
-    
+
     def __iter__(self):
         return iter(self._tests)
 
     def get_test_result(self, uid):
         return self.get_test(uid)
-    
+
     def aggregate_test_results(self):
         results = {}
         for test in self:
@@ -110,32 +110,32 @@ class InternalLibraryResults(object, _CommonMetadataMixin):
     def __init__(self, obj, directory):
         self.directory = directory
         self._metadata = obj.metadata
-        self._wrap_suites(obj)   
+        self._wrap_suites(obj)
 
     def __iter__(self):
         return iter(self._suites)
 
     def _wrap_suites(self, obj):
-        self._suites = [InternalSuiteResult(suite, self.directory) 
+        self._suites = [InternalSuiteResult(suite, self.directory)
                         for suite in obj]
         self._suites_index = _create_uid_index(self._suites)
-    
+
     def add_suite(self, suite):
         if suite.uid in self._suites:
             raise ValueError('Cannot have duplicate suite UIDs.')
         self._suites[suite.uid] = suite
-    
+
     def get_suite_result(self, suite_uid):
         return self._suites_index[suite_uid]
 
     def get_test_result(self, test_uid, suite_uid):
         return self.get_suite_result(suite_uid).get_test_result(test_uid)
-    
+
     def aggregate_test_results(self):
         results = {}
         for suite in self._suites:
             for test in suite:
-                helper.append_dictlist(results, test.result.value, test)   
+                helper.append_dictlist(results, test.result.value, test)
         return results
 
 class InternalSavedResults:
@@ -149,7 +149,7 @@ class InternalSavedResults:
             base = config.result_path
         return os.path.join(
                 base,
-                str(suite_uid).replace(os.path.sep, '-'), 
+                str(suite_uid).replace(os.path.sep, '-'),
                 str(test_uid).replace(os.path.sep, '-'))
 
     @staticmethod
@@ -193,7 +193,7 @@ class XMLAttribute(object):
         self.value = value
 
     def write(self, file_):
-        file_.write('%s=%s' % (self.name, 
+        file_.write('%s=%s' % (self.name,
                 xml.sax.saxutils.quoteattr(self.value)))
 
 
@@ -207,7 +207,7 @@ class JUnitTestSuites(XMLElement):
 
     def __init__(self, internal_results):
         results = internal_results.aggregate_test_results()
-        
+
         self.attributes = []
         for result, tests in results.items():
             self.attributes.append(self.result_attribute(result, str(len(tests))))
@@ -230,7 +230,7 @@ class JUnitTestSuite(JUnitTestSuites):
 
     def __init__(self, suite_result):
         results = suite_result.aggregate_test_results()
-        
+
         self.attributes = [
             XMLAttribute('name', suite_result.name)
         ]
@@ -253,7 +253,7 @@ class JUnitTestCase(XMLElement):
             XMLAttribute('status', str(test_result.result)),
         ]
 
-        # TODO JUnit expects a message for the reason a test was 
+        # TODO JUnit expects a message for the reason a test was
         # skipped or errored, save this with the test metadata.
         # http://llg.cubic.org/docs/junit/
         self.elements = [
@@ -273,11 +273,11 @@ class LargeFileElement(XMLElement):
                 for line in f:
                     file_.write(xml.sax.saxutils.escape(line))
         except IOError:
-            # TODO Better error logic, this is sometimes O.K. 
+            # TODO Better error logic, this is sometimes O.K.
             # if there was no stdout/stderr captured for the test
             #
             # TODO If that was the case, the file should still be made and it should just be empty instead of not existing.
-            log.test_log.debug('Error reading from %s' % self.filename)
+            pass
 
 
 
@@ -290,4 +290,4 @@ class JUnitSavedResults:
         results = JUnitTestSuites(results)
         with open(path, 'w') as f:
             results.write(f)
-    
+
