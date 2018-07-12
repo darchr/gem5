dc9dcba2d53e64a0739d05d3ff10038d26eee35d
Jason Lowe-Power <jason@lowepower.com>
2018-07-12 15:06:45 -0700

    tests: Add tests for cpus
    Add a simple test of some small applications from the Stanford suite.
    The code comes from the LLVM test suite.
    test-suite/tree/master/SingleSource/Benchmarks/Stanford
    
    Change-Id: I4fca4f2ec4aabd452f55901c0a6092fac6c476be
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ tests/gem5/cpu_tests/run.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 The Regents of the University of California
+# All Rights Reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Jason Lowe-Power
+
+import os
+import argparse
+
+import m5
+from m5.objects import *
+
+class MySimpleMemory(SimpleMemory):
+    latency = '1ns'
+
+valid_cpu = {'TimingSimpleCPU': TimingSimpleCPU,
+            }
+valid_mem = {'SimpleMemory': MySimpleMemory,
+             'DDR3_1600_8x8': DDR3_1600_8x8
+            }
+
+parser = argparse.ArgumentParser()
+parser.add_argument('binary', type = str)
+parser.add_argument('--cpu', choices = valid_cpu.keys(),
+                    default = 'TimingSimpleCPU')
+parser.add_argument('--mem', choices = valid_mem.keys(),
+                    default = 'SimpleMemory')
+
+args = parser.parse_args()
+
+system = System()
+
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('512MB')]
+
+# TODO Add options to change this!
+system.cpu = valid_cpu[args.cpu]()
+
+system.membus = SystemXBar()
+system.cpu.icache_port = system.membus.slave
+system.cpu.dcache_port = system.membus.slave
+
+system.cpu.createInterruptController()
+if m5.defines.buildEnv['TARGET_ISA'] == "x86":
+    system.cpu.interrupts[0].pio = system.membus.master
+    system.cpu.interrupts[0].int_master = system.membus.slave
+    system.cpu.interrupts[0].int_slave = system.membus.master
+
+# TODO Add option to change this.
+system.mem_ctrl = valid_mem[args.mem]()
+system.mem_ctrl.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.master
+system.system_port = system.membus.slave
+
+process = Process()
+process.cmd = [args.binary]
+system.cpu.workload = process
+system.cpu.createThreads()
+
+root = Root(full_system = False, system = system)
+m5.instantiate()
+
+exit_event = m5.simulate()
+
+if exit_event.getCause() != 'exiting with last active thread context':
+    exit(1)
