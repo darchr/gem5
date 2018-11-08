95fcc8682eaa8f8bd6ef6dd683c65e3d1fe00869
Jason Lowe-Power <jason@lowepower.com>
2018-11-08 14:01:25 -0800

    Revert "Instport completed/defined"
    This reverts commit 70413e1e5cbe0a9245d023808d97fa75c091f350.

--- src/verilator/verilator_object.cc
+++ src/verilator/verilator_object.cc
@@ -103,7 +103,6 @@ VerilatorObject::sendFetch(const RequestPtr &req)
 
         instRequested = true;
         //assertn io.imem.resp.valid response to false to insert bubbles
-        Top__DOT__tile__DOT__core__DOT__c_io_ctl_stall = 1;
 }
 
 
@@ -121,7 +120,6 @@ VerilatorObject::handleResponse(PacketPtr pkt)
         dut.Top__DOT__tile__DOT__memory__DOT__async_data_dataInstr_1_data =
                 respData[3] | respData[2] | respData[1] | respData[0];
         //set valid to true
-        Top__DOT__tile__DOT__core__DOT__c_io_ctl_stall = 0;
         //no imem valid signal. set stall instead?
         instRequested = false;
     } else  if (pkt->req->isRead()){
@@ -175,7 +173,6 @@ VerilatorObjectParams::create()
 {
     VerilatorObject * vo = new VerilatorObject(this);
     return vo;
-
 }
 
 void
@@ -192,7 +189,7 @@ VerilatorObject::processEvent()
         setupFetchRequest(ifetch_req);
         sendFetch(ifetch_req);
     }else if (instRequested){
-        dut.Top__DOT__tile__DOT__core__DOT__c_io_ctl_stall = 1;
+        //stall
     }
 
     if (Top__DOT__tile__DOT__memory__DOT__io_core_ports_0_req_valid
