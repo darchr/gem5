c00a97f8b84344c9c9580c2b70bf6293b0cbf341
Jason Lowe-Power <jason@lowepower.com>
2018-11-11 17:10:25 -0800

    verilator: Update verilog to be 32 bits of addr
    Change-Id: Ib3b21c2765640a5db09b28692901536dac2ad881
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- ext/sodor/AsyncReadMem.sv
+++ ext/sodor/AsyncReadMem.sv
@@ -1,28 +1,26 @@
 module AsyncReadMem
-  #(parameter NUM_BYTES = (1 << 21),
-    parameter DATA_WIDTH = 32)
+  #(parameter DATA_WIDTH = 32)
    (input clk,
 
-    input [ADDR_WIDTH-1:0] hw_addr,
+    input [31:0] hw_addr,
     input [DATA_WIDTH-1:0] hw_data,
     input [MASK_WIDTH-1:0] hw_mask,
     input hw_en,
 
-    input [ADDR_WIDTH-1:0] hr_addr,
+    input [31:0] hr_addr,
     output [DATA_WIDTH-1:0] hr_data,
 
-    input [ADDR_WIDTH-1:0] dw_addr,
+    input [31:0] dw_addr,
     input [DATA_WIDTH-1:0] dw_data,
     input [MASK_WIDTH-1:0] dw_mask,
     input dw_en,
 
-    input [ADDR_WIDTH-1:0] dataInstr_0_addr,
+    input [31:0] dataInstr_0_addr,
     output [DATA_WIDTH-1:0] dataInstr_0_data,
-    input [ADDR_WIDTH-1:0] dataInstr_1_addr,
+    input [31:0] dataInstr_1_addr,
     output [DATA_WIDTH-1:0] dataInstr_1_data
     );
 
-   localparam ADDR_WIDTH = $clog2(NUM_BYTES);
    localparam MASK_WIDTH = DATA_WIDTH >> 3;
 
 /*   reg [7:0] 		mem [0:NUM_BYTES-1];
