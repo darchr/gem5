188adb620aa46dc3e3bdf38bbc5003e9b349572a
root <root@amarillo.cs.ucdavis.edu>
2019-03-14 14:14:29 -0700

    fixed SConscript and CPU config. Should compile now
    Change-Id: I5662dd2404ea59619e05974fa853c41e20b232d2

--- src/verilator/VerilatorDinoCPU.py
+++ src/verilator/VerilatorDinoCPU.py
@@ -29,7 +29,7 @@
 
 
 from m5.params import *
-fomm VerilatorMemBlackBox import *
+from VerilatorMemBlackBox import VerilatorMemBlackBox
 from m5.SimObject import SimObject
 
 class VerilatorDinoCPU(SimObject):
@@ -38,5 +38,5 @@ class VerilatorDinoCPU(SimObject):
 
     clkperiod = Param.Latency("Clock period of device under test")
     stages = Param.Int(1, "Number of stages in the device under test")
-    verilatorMem = Param.VerilatorMemBlackBox(NULL, "Reference to
-        verilator memory interface")
+    verilatorMem = Param.VerilatorMemBlackBox(NULL, "Reference to "
+        "verilator memory interface")
