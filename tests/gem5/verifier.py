ace01c33228082ff8695769786f80a257cc4ce77
Cazamere <clcomrie@ucdavis.edu>
2019-11-07 15:03:54 -0800

    verifier: added newline
    Change-Id: I14d665ed3e6029c8546e1c36afe6eb232a467e8f

--- tests/gem5/verifier.py
+++ tests/gem5/verifier.py
@@ -69,7 +69,6 @@ class MatchGoldStandard(Verifier):
         '''
         :param standard_filename: The path of the standard file to compare
         output to.
-
         :param ignore_regex: A string, compiled regex, or iterable containing
         either which will be ignored in 'standard' and test output files when
         diffing.
@@ -91,13 +90,7 @@ class MatchGoldStandard(Verifier):
                             self.test_filename,
                             ignore_regexes=self.ignore_regex,
                             logger=params.log)
-        '''
-        for line in diff:
-            if "committedInsts" in line:
-                self.failed(fixtures)
-                test.fail('Stats did not match:\n%s\nSee %s for full results'
-                      % (diff, tempdir))
-        '''
+
         if diff is not None:
             self.failed(fixtures)
             test.fail('Stdout did not match:\n%s\nSee %s for full results'
@@ -112,6 +105,29 @@ class MatchGoldStandard(Verifier):
             raise ValueError('If you are setting test_filename use the more'
                              ' generic %s'
                              ' instead' % MatchGoldStandard.__name__)
+
+class MatchStat(MatchGoldStandard):
+    def __init__(self, stat):
+        self.stats = stat
+
+    def test(self, params):
+        # We need a tempdir fixture from our parent verifier suite.
+        fixtures = params.fixtures
+        # Get the file from the tempdir of the test.
+        tempdir = fixtures[constants.tempdir_fixture_name].path
+        self.test_filename = joinpath(tempdir, self.test_filename)
+
+        diff = diff_out_file(self.standard_filename,
+                            self.test_filename,
+                            ignore_regexes=self.ignore_regex,
+                            logger=params.log)
+
+        for line in diff:
+            for stat in self.stats:
+                if stat in line:
+                    self.failed(fixtures)
+                    test.fail('Stats did not match:\n%s\nSee %s for results'
+                        % (diff, tempdir))
 
 class DerivedGoldStandard(MatchGoldStandard):
     __ignore_regex_sentinel = object()
