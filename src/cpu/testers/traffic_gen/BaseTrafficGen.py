b29aa539273b3a0dd5fa282af29f6d554a883a8f
LakinS <lakin@amarillo.cs.ucdavis.edu>
2020-07-30 16:01:04 -0700

    Revert "misc: Changed 'master/slave' terminology"
    This reverts commit abe6e798545f3e09af7aa56f7cd2cbf2469f343b.

--- src/cpu/testers/traffic_gen/BaseTrafficGen.py
+++ src/cpu/testers/traffic_gen/BaseTrafficGen.py
@@ -110,9 +110,9 @@ class BaseTrafficGen(ClockedObject):
     def connectCachedPorts(self, bus):
         if hasattr(self, '_cached_ports') and (len(self._cached_ports) > 0):
             for p in self._cached_ports:
-                exec('self.%s = bus.responder' % p)
+                exec('self.%s = bus.slave' % p)
         else:
-            self.port = bus.responder
+            self.port = bus.slave
 
     def connectAllPorts(self, cached_bus, uncached_bus = None):
         self.connectCachedPorts(cached_bus)
