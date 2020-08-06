d92c126afc388495fbebf95fc066d02f325be06b
eavivi <eavivi@ucdavis.edu>
2020-08-06 13:15:25 -0700

    gpu-compute: MasterID to UniqueID
    Change-Id: I2acc5741dff576d33cb10eb6e624cd3167b3a350

--- src/gpu-compute/compute_unit.hh
+++ src/gpu-compute/compute_unit.hh
@@ -463,13 +463,13 @@ class ComputeUnit : public ClockedObject
     void processFetchReturn(PacketPtr pkt);
     void updatePageDivergenceDist(Addr addr);
 
-    MasterID masterId() { return _masterId; }
+    UniqueID masterId() { return _masterId; }
 
     bool isDone() const;
     bool isVectorAluIdle(uint32_t simdId) const;
 
   protected:
-    MasterID _masterId;
+    UniqueID _masterId;
 
     LdsState &lds;
 
