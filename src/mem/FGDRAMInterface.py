e1716590837473a3ebbb44f7d293f2338b3d4d34
Marjan Fariborz <mfariborz@ucdavis.edu>
2021-04-02 10:54:59 -0700

    mem: Adding interface params for FGDRAM.
    Change-Id: I6c4f9fa279c50e43a1669956a27c111d6c7eb0f2

--- src/mem/FGDRAMInterface.py
+++ src/mem/FGDRAMInterface.py
@@ -26,62 +26,48 @@
 
 from m5.objects.DRAMInterface import PageManage, DRAMInterface
 
-class LLM(DRAMInterface):
+class FGDRAM(DRAMInterface):
 
-    # SerDes working at 32GHz, 32GHz x 2ns = 64
-    beats_per_clock = 64
+    device_bus_width = 64
 
-    # 500 MHz for 2GB/s per Die
-    tCK = '2ns'
+    # HBM supports BL4 and BL2 (legacy mode only)
+    burst_length = 4
 
-    # Each channel connecting to a single optical IO
-    device_bus_width = 1
+    # size of channel in bytes, 4H stack of 2Gb dies is 1GB per stack;
+    # with 8 channels, 128MB per channel
+    device_size = '8MB'
 
-    # Using close-page  policy to  have deterministic
-    # memory access latency
-    page_policy = 'close'
-
-    # Sending 64bytes every 16ns (tBURST)
-    # BL = 16ns / 2ns (tCK) = 8
-    burst_length = 512
-
-    # Size of channel in bytes, 4H stack of 8Gb dies is 4GB per stack.
-    # 16 channels (memscheduler) For 64 Bank per channel.
-    device_size = '4MB'
-
-    # Row buffer size for  HBM2.0 is 1KB
-    # To save activation energy we divide this by half
-    # To conpensate for the bank BW we can increase the bitlines
     device_rowbuffer_size = '256B'
 
+    # 1x128 configuration
     devices_per_rank = 1
 
+    # HBM does not have a CS pin; set rank to 1
     ranks_per_channel = 1
 
-    # Hack: each bank has 2 mini-banks
+    # HBM has 8 or 16 banks depending on capacity
+    # 2Gb dies have 8 banks
     banks_per_rank = 2
 
-    # One bank group per bank
+    # depending on frequency, bank groups may be required
+    # will always have 4 bank groups when enabled
+    # current specifications do not define the minimum frequency for
+    # bank group architecture
+    # setting bank_groups_per_rank to 0 to disable until range is defined
     bank_groups_per_rank = 0
+
+    # 500 MHz for 1Gbps DDR data rate
+    tCK = '2ns'
 
     # use values from IDD measurement in JEDEC spec
     # use tRP value for tRCD and tCL similar to other classes
     tRP = '15ns'
-    # Decreasing the length of global bitline by a factor of 2.
-    tRCD = '8ns'
-    # Pre-GSA tCL + post-GSA tCL
-    tCL = '5ns'
-    tRAS = '15ns'
-    # We consider tCCD_L here as tRCD
-    # Requests going to the same bank and different mini-banks
-    # should be tRCD apart
-    # We are doing this because we are modeling mini0banks with
-    # multiple banks per ranks
-    tCCD_L = '15ns'
+    tRCD = '15ns'
+    tCL = '15ns'
+    tRAS = '33ns'
 
-    tBURST_MIN = '2ns'
-    tBURST_MAX = '4ns'
-    #  64Byte (atom size) / 32Gbps (datarate of wavelength) = 16ns
+    # BL2 and BL4 supported, default to BL4
+    # DDR @ 500 MHz means 4 * 2ns / 2 = 4ns
     tBURST = '16ns'
 
     # value for 2Gb device from JEDEC spec
@@ -90,26 +76,24 @@ class LLM(DRAMInterface):
     # value for 2Gb device from JEDEC spec
     tREFI = '3.9us'
 
-
+    # extrapolate the following from LPDDR configs, using ns values
+    # to minimize burst length, prefetch differences
     tWR = '18ns'
-    # The time between read to precharge
-    # ~ tRAS - tRCD
-    tRTP = '7ns'
-    # No bus turnaround. One waveguide per read and one per write.
-    tWTR = '0ns'
+    tRTP = '7.5ns'
+    tWTR = '10ns'
 
-    # Same as above: busturnaround penalty (read-to-write, write-to-read)
-    tRTW = '0ns'
+    # start with 2 cycles turnaround, similar to other memory classes
+    # could be more with variations across the stack
+    tRTW = '4ns'
+
     # single rank device, set to 0
     tCS = '0ns'
 
     # from MemCon example, tRRD is 4ns with 2ns tCK
-    tRRD = '2ns'
-    tRRD_L = '2ns'
+    tRRD = '4ns'
 
+    # from MemCon example, tFAW is 30ns with 2ns tCK
     tXAW = '12ns'
-    # Reduced the activation row size by a factor of 4.
-    # We can have 4 times more acivations.
     activation_limit = 32
 
     # 4tCK
@@ -117,5 +101,3 @@ class LLM(DRAMInterface):
 
     # start with tRFC + tXP -> 160ns + 8ns = 168ns
     tXS = '168ns'
-
-    addr_mapping = 'RoCoRaBaCh'
