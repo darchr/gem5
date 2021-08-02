8cfd3cccb3d751089277612d40d110fd2b4f7dea
Laura Hinman <llhinman@ucdavis.edu>
2021-08-02 11:14:30 -0700

    dev,arch-riscv: Introduced new LupV platform
    This is a RISCV-based platform derived from the HiFive platform
    containing the LupIO devices and will continue to be modified as more
    devices are introduced. The LupIO devices are a collection of processor
    agnostic and easily implemented virtual devices. Details about the
    specifications of all eight LupIO devices can be found here:
    https://gitlab.com/luplab/lupio/lupio-specs/-/tree/main
    
    Information about how to build a RISCV full system with the LupIO-RTC
    can be found here:
    https://github.com/darchr/lupio-gem5/tree/configs
    
    Change-Id: Ie15a217c9ab4e33d112acc3f498ba5b2eb4bb7d5

---
+++ src/dev/riscv/LupV.py
@@ -0,0 +1,207 @@
+ # Copyright (c) 2021 The Regents of the University of California
+ # All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions are
+ # met: redistributions of source code must retain the above copyright
+ # notice, this list of conditions and the following disclaimer;
+ # redistributions in binary form must reproduce the above copyright
+ # notice, this list of conditions and the following disclaimer in the
+ # documentation and/or other materials provided with the distribution;
+ # neither the name of the copyright holders nor the names of its
+ # contributors may be used to endorse or promote products derived from
+ # this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ # A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ # OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ # LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ # DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from m5.objects.Clint import Clint
+from m5.objects.LupioRTC import LupioRTC
+from m5.objects.Platform import Platform
+from m5.objects.Plic import Plic
+from m5.objects.Terminal import Terminal
+from m5.objects.Uart import Uart8250
+from m5.params import Param
+from m5.params import AddrRange
+from m5.util.fdthelper import *
+
+class LupV(Platform):
+    """LupV Platform copied from dev/riscv/HiFive.py
+    This platform will continually be upadated to
+    incorporate the LupIO devices.
+
+    Implementation:
+        This is the base class for the LupV board
+        series. It contains the CLINT and PLIC interrupt
+        controllers, LupioRTC, Uart and Disk.
+
+    Setup:
+        The following sections outline the required
+        setup for a RISC-V LupV platform. See
+        configs/example/riscv/fs_linux.py for example.
+
+    Driving CLINT:
+        CLINT has an interrupt pin which increments
+        mtime. It can be connected to any interrupt
+        source pin which acts as the RTCCLK pin. An
+        abstract RTC wrapper called RiscvRTC can be
+        used.
+
+    Attaching PLIC devices:
+        PLIC handles external interrupts. Interrupt
+        PioDevices should inherit from PlicIntDevice
+        (PCI and DMA not yet implemented). It contains
+        a parameter interrupt_id which should be used
+        to call platform->postPciInt(id).
+        All PLIC interrupt devices should be returned
+        by _off_chip_devices(). Calling attachPlic sets
+        up the PLIC interrupt source count.
+
+    LupioRTC:
+        The LupioRTC is a real time clock that supplies
+        the current date and time.
+
+    Uart:
+        The LupV platform also has an uart_int_id.
+        This is because Uart8250 uses postConsoleInt
+        instead of postPciInt. In the future if a Uart
+        that inherits PlicIntDevice is implemented,
+        this can be removed.
+
+    Disk:
+        See fs_linux.py for setup example.
+
+    PMAChecker:
+        The PMAChecker will be attached to the MMU of
+        each CPU (which allows them to differ). See
+        fs_linux.py for setup example.
+    """
+
+    type = 'LupV'
+    cxx_header = "dev/riscv/lupv.hh"
+    cxx_class = 'gem5::LupV'
+
+    # CLINT
+    clint = Param.Clint(Clint(pio_addr=0x2000000), "CLINT")
+
+    # PLIC
+    plic = Param.Plic(Plic(pio_addr=0xc000000), "PLIC")
+
+    # LUPIO RTC
+    lupio_rtc = Param.LupioRTC(LupioRTC(pio_addr=0x20004000), "LupioRTC")
+
+    # Uart
+    uart = Uart8250(pio_addr=0x10000000)
+    # Int source ID to redirect console interrupts to
+    # Set to 0 if using a pci interrupt for Uart instead
+    uart_int_id = Param.Int(0xa, "PLIC Uart interrupt ID")
+    terminal = Terminal()
+
+    # Dummy param for generating devicetree
+    cpu_count = Param.Int(0, "dummy")
+
+    def _on_chip_devices(self):
+        """Returns a list of on-chip peripherals
+        """
+        return [
+            self.clint,
+            self.plic
+        ]
+
+    def _off_chip_devices(self):
+        """Returns a list of off-chip peripherals
+        """
+        devices = [
+            self.uart,
+            self.lupio_rtc
+        ]
+
+        if hasattr(self, "disk"):
+            devices.append(self.disk)
+        return devices
+
+    def _on_chip_ranges(self):
+        """Returns a list of on-chip peripherals
+            address range
+        """
+        return [
+            AddrRange(dev.pio_addr, size=dev.pio_size)
+            for dev in self._on_chip_devices()
+        ]
+
+    def _off_chip_ranges(self):
+        """Returns a list of off-chip peripherals
+            address range
+        """
+        return [
+            AddrRange(dev.pio_addr, size=dev.pio_size)
+            for dev in self._off_chip_devices()
+        ]
+
+    def attachPlic(self):
+        """Count number of PLIC interrupt sources
+        """
+        plic_srcs = [self.uart_int_id]
+        for device in self._off_chip_devices():
+            if hasattr(device, "interrupt_id"):
+                plic_srcs.append(device.interrupt_id)
+        self.plic.n_src = max(plic_srcs) + 1
+
+    def attachOnChipIO(self, bus):
+        """Attach on-chip IO devices, needs modification
+            to support DMA and PCI
+        """
+        for device in self._on_chip_devices():
+            device.pio = bus.mem_side_ports
+
+    def attachOffChipIO(self, bus):
+        """Attach off-chip IO devices, needs modification
+            to support DMA and PCI
+        """
+        for device in self._off_chip_devices():
+            device.pio = bus.mem_side_ports
+
+    def generateDeviceTree(self, state):
+        cpus_node = FdtNode("cpus")
+        cpus_node.append(FdtPropertyWords("timebase-frequency", [10000000]))
+        yield cpus_node
+
+        node = FdtNode("soc")
+        local_state = FdtState(addr_cells=2, size_cells=2)
+        node.append(local_state.addrCellsProperty())
+        node.append(local_state.sizeCellsProperty())
+        node.append(FdtProperty("ranges"))
+        node.appendCompatible(["simple-bus"])
+
+        for subnode in self.recurseDeviceTree(local_state):
+            node.append(subnode)
+
+        yield node
+
+    def annotateCpuDeviceNode(self, cpu, state):
+        cpu.append(FdtPropertyStrings('mmu-type', 'riscv,sv48'))
+        cpu.append(FdtPropertyStrings('status', 'okay'))
+        cpu.append(FdtPropertyStrings('riscv,isa', 'rv64imafdcsu'))
+        cpu.appendCompatible(["riscv"])
+
+        int_node = FdtNode("interrupt-controller")
+        int_state = FdtState(interrupt_cells=1)
+        int_node.append(int_state.interruptCellsProperty())
+        int_node.append(FdtProperty("interrupt-controller"))
+        int_node.appendCompatible("riscv,cpu-intc")
+
+        cpus = self.system.unproxy(self).cpu
+        phandle = int_state.phandle(cpus[self.cpu_count])
+        self.cpu_count += 1
+        int_node.append(FdtPropertyWords("phandle", [phandle]))
+
+        cpu.append(int_node)
---
+++ src/dev/riscv/lupv.cc
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2021 The Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dev/riscv/lupv.hh"
+
+#include "dev/lupio/lupio_rtc.hh"
+#include "dev/riscv/clint.hh"
+#include "dev/riscv/plic.hh"
+#include "params/LupV.hh"
+
+namespace gem5
+{
+
+using namespace RiscvISA;
+
+LupV::LupV(const Params &params) :
+    Platform(params),
+    clint(params.clint),
+    lupioRTC(params.lupio_rtc),
+    plic(params.plic),
+    uartIntID(params.uart_int_id)
+{
+}
+
+void
+LupV::postConsoleInt()
+{
+    plic->post(uartIntID);
+}
+
+void
+LupV::clearConsoleInt()
+{
+    plic->clear(uartIntID);
+}
+
+void
+LupV::postPciInt(int line)
+{
+    plic->post(line);
+}
+
+void
+LupV::clearPciInt(int line)
+{
+    plic->clear(line);
+}
+
+Addr
+LupV::pciToDma(Addr pciAddr) const
+{
+    panic("LupV::pciToDma() has not been implemented.");
+}
+
+void
+LupV::serialize(CheckpointOut &cp) const
+{
+}
+
+void
+LupV::unserialize(CheckpointIn &cp)
+{
+}
+
+} // namespace gem5
