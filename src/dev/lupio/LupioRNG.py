302c799fd7f53b1f6794ec4b23fdb187b358b7fc
Melissa Jost <melissakjost@gmail.com>
2021-08-16 12:29:22 -0700

    WIP: Added new LupIO-RNG device
    Change-Id: Ia6aeb610ebe5589ed1f1548b823c5165236b03e6

--- src/dev/lupio/LupioRNG.py
+++ src/dev/lupio/LupioRNG.py
@@ -24,13 +24,35 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Import('*')
+from m5.objects.Device import BasicPioDevice
+from m5.params import Param
+from m5.proxy import Parent
+from m5.defines import buildEnv
+from m5.util.fdthelper import FdtPropertyWords
 
-SimObject('LupioRNG.py')
-SimObject('LupioRTC.py')
+class LupioRNG(BasicPioDevice):
 
-DebugFlag('LupioRNG')
-DebugFlag('LupioRTC')
+    type = 'LupioRNG'
+    cxx_class='gem5::LupioRNG'
+    cxx_header = 'dev/lupio/lupio_rng.hh'
+    pio_size = Param.Addr(0x1000, "PIO Size")
+    seed = Param.Int(0, "Initial seed for the random number generator")
+    if buildEnv['TARGET_ISA'] == 'riscv':
+        plic = Param.Plic(Parent.any, "PLIC")
+        lupio_rng_int_id = Param.Int(3, "Interrupt ID for the RNG")
 
-Source('lupio_rng.cc')
-Source('lupio_rtc.cc')
+    def generateDeviceTree(self, state):
+
+        if buildEnv['TARGET_ISA'] != 'riscv':
+            return
+
+        node = self.generateBasicPioDeviceNode(state, "lupio-rng",
+                                               self.pio_addr,
+                                               self.pio_size)
+        plic = self.plic
+        node.append(
+                FdtPropertyWords("interrupts", [self.lupio_rng_int_id]))
+        node.append(
+                FdtPropertyWords("interrupt-parent", state.phandle(plic)))
+        node.appendCompatible(["lupio,rng"])
+        yield node
