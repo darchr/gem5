e59c50d61c5bbcaaa1baba64336133b49ee82233
Laura Hinman <llhinman@ucdavis.edu>
2021-08-17 17:16:04 -0700

    WIP: Added new LupIO-TTY device
    This device is notfied when data is available from the terminal
    (e.g. from keyboard input) in order to receive characters. It also
    transmits characters to the terminal to be displayed.
    
    Change-Id: Icc8294984989cfa422d8ed227da39debfa49ab36

--- src/dev/lupio/LupioTTY.py
+++ src/dev/lupio/LupioTTY.py
@@ -24,16 +24,18 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Import('*')
+from m5.params import Param
+from m5.proxy import *
+from m5.util.fdthelper import *
 
-SimObject('LupioRNG.py')
-SimObject('LupioRTC.py')
-SimObject('LupioTTY.py')
+from m5.objects.Device import BasicPioDevice
+from m5.objects.Serial import SerialDevice
 
-DebugFlag('LupioRNG')
-DebugFlag('LupioRTC')
-DebugFlag('LupioTTY')
-
-Source('lupio_rng.cc')
-Source('lupio_rtc.cc')
-Source('lupio_tty.cc')
+class LupioTTY(BasicPioDevice):
+    type = 'LupioTTY'
+    cxx_class = 'gem5::LupioTTY'
+    cxx_header = "dev/lupio/lupio_tty.hh"
+    terminal = Param.SerialDevice(Parent.any, "The terminal")
+    pio_size = Param.Addr(0x1000, "PIO size")
+    platform = Param.Platform(Parent.any,
+                              "Platform this device is part of.")
