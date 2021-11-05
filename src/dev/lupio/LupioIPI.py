ea190fc7f9771499a8e48f86ae9b7629e231231b
Melissa Jost <melissakjost@gmail.com>
2021-11-05 16:28:01 -0700

    dev: Added new Lupio-IPI device
    Added the LupIO inter-processor interrupt controller
    device which will allow for us to use an SMP system
    with the LupIO devices.
    
    Change-Id: Iceab7446b36fb4d9b7605f3ba28665fca509d55d

--- src/dev/lupio/LupioIPI.py
+++ src/dev/lupio/LupioIPI.py
@@ -24,28 +24,14 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Import('*')
+from m5.objects.Device import BasicPioDevice
+from m5.params import Param
 
-SimObject('LupioBLK.py')
-SimObject('LupioIPI.py')
-SimObject('LupioPIC.py')
-SimObject('LupioRNG.py')
-SimObject('LupioRTC.py')
-SimObject('LupioTMR.py')
-SimObject('LupioTTY.py')
+class LupioIPI(BasicPioDevice):
+    type = 'LupioIPI'
+    cxx_class='gem5::LupioIPI'
+    cxx_header = 'dev/lupio/lupio_ipi.hh'
+    pio_size = Param.Addr(0x1000, "PIO Size")
+    int_type = Param.Int("Type of interrupt")
+    num_threads = Param.Int("Number of threads")
 
-DebugFlag('LupioBLK')
-DebugFlag('LupioIPI')
-DebugFlag('LupioPIC')
-DebugFlag('LupioRNG')
-DebugFlag('LupioRTC')
-DebugFlag('LupioTMR')
-DebugFlag('LupioTTY')
-
-Source('lupio_blk.cc')
-Source('lupio_ipi.cc')
-Source('lupio_pic.cc')
-Source('lupio_rng.cc')
-Source('lupio_rtc.cc')
-Source('lupio_tmr.cc')
-Source('lupio_tty.cc')
