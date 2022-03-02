13a2fdb11ba67ff8a880864e295b1440a9df7bac
Jerin Joy <joy@rivosinc.com>
2022-03-02 15:44:26 -0800

    HACK: rivos: Resize VecRegs to RiscvISA::NumVecRegs
    Signed-off-by: Jerin Joy <joy@rivosinc.com>

--- src/cpu/simple_thread.cc
+++ src/cpu/simple_thread.cc
@@ -80,7 +80,7 @@ SimpleThread::SimpleThread(BaseCPU *_cpu, int _thread_num, System *_sys,
     const auto &regClasses = isa->regClasses();
     intRegs.resize(regClasses.at(IntRegClass).size());
     floatRegs.resize(regClasses.at(FloatRegClass).size());
-    vecRegs.resize(regClasses.at(VecRegClass).size());
+    vecRegs.resize(RiscvISA::NumVecRegs);
     vecElemRegs.resize(regClasses.at(VecElemClass).size());
     vecPredRegs.resize(regClasses.at(VecPredRegClass).size());
     ccRegs.resize(regClasses.at(CCRegClass).size());
