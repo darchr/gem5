8ecf58b5d12c7b91a6b223f24eb6cf4deca944be
Mahyar Samani <msamani@ucdavis.edu>
2022-04-24 20:28:25 -0700

    A little bit of debugging and updating config script.

--- configs/accl/sega.py
+++ configs/accl/sega.py
@@ -1,5 +1,9 @@
 import m5
+import argparse
+
+from math import log
 from m5.objects import *
+from m5.util.convert import toMemorySize
 
 class MPU(SubSystem):
     def __init__(self, base_edge_addr):
@@ -35,53 +39,115 @@ class MPU(SubSystem):
         self.push_engine.mem_port = port
 
 class MPUMemory(SubSystem):
-    def __init__(self, vertex_range, vertex_binary, edge_range, edge_binary):
+    def __init__(self,
+                    num_channels: int,
+                    cache_line_size: int,
+                    vertex_memory_size: str,
+                    edge_memory_size: str,
+                    graph_path: str):
         super(MPUMemory, self).__init__()
-        self.vertex_mem_ctrl = SimpleMemory(
-            range=vertex_range, bandwidth="19.2GB/s",
-            latency="30ns", image_file=vertex_binary)
-        self.edge_mem_ctrl = SimpleMemory(
-            range=edge_range, bandwidth="19.2GB/s",
-            latency="30ns", image_file=edge_binary)
 
-    def getVertexPort(self):
-        return self.vertex_mem_ctrl.port
-    def setVertexPort(self, port):
-        self.vertex_mem_ctrl.port = port
+        self._vertex_ranges = self._interleave_addresses(
+                                AddrRange(start=0, size=vertex_memory_size),\
+                                num_channels,\
+                                cache_line_size)
 
-    def getEdgePort(self):
-        return self.edge_mem_ctrl.port
-    def setEdgePort(self, port):
-        self.edge_mem_ctrl.port = port
+        self._edge_chunk_size = int(\
+                                toMemorySize(edge_memory_size)/num_channels)
+        self._edge_ranges = [AddrRange(\
+                            start=toMemorySize(vertex_memory_size)+\
+                            self._edge_chunk_size*i,\
+                            size=self._edge_chunk_size)\
+                            for i in range(num_channels)]
+
+        vertex_mem_ctrl = []
+        edge_mem_ctrl = []
+        for i in range(num_channels):
+            vertex_mem_ctrl.append(
+                SimpleMemory(range=self._vertex_ranges[i],
+                            bandwidth="19.2GB/s",
+                            latency="30ns",
+                            image_file=f"{graph_path}/vertices_{i}")
+            )
+            edge_mem_ctrl.append(
+                SimpleMemory(range=self._edge_ranges[i],
+                            bandwidth="19.2GB/s",
+                            latency="30ns",
+                            image_file=f"{graph_path}/edgelist_{i}")
+            )
+        self.vertex_mem_ctrl = vertex_mem_ctrl
+        self.edge_mem_ctrl = edge_mem_ctrl
+
+    def _interleave_addresses(self,
+                            plain_range,
+                            num_channels,
+                            cache_line_size):
+        intlv_low_bit = log(cache_line_size, 2)
+        intlv_bits = log(num_channels, 2)
+        ret = []
+        for i in range(num_channels):
+            ret.append(AddrRange(
+                start=plain_range.start,
+                size=plain_range.size(),
+                intlvHighBit=intlv_low_bit + intlv_bits - 1,
+                xorHighBit=0,
+                intlvBits=intlv_bits,
+                intlvMatch=i))
+        return ret
+
+    def getVertexPort(self, i):
+        return self.vertex_mem_ctrl[i].port
+    def setVertexPort(self, port, i):
+        self.vertex_mem_ctrl[i].port = port
+
+    def getEdgeBaseAddr(self, i):
+        return self._edge_ranges[i].start
+    def getEdgePort(self, i):
+        return self.edge_mem_ctrl[i].port
+    def setEdgePort(self, port, i):
+        self.edge_mem_ctrl[i].port = port
 
 class SEGA(System):
-    def __init__(self):
+    def __init__(self, num_mpus, graph_path):
         super(SEGA, self).__init__()
         self.clk_domain = SrcClockDomain()
         self.clk_domain.clock = '1GHz'
         self.clk_domain.voltage_domain = VoltageDomain()
 
+        self.interconnect = NoncoherentXBar(frontend_latency=1,
+                                            forward_latency=1,
+                                            response_latency=1,
+                                            width=64)
+
         self.ctrl = CenteralController(addr=0, value=0)
-        self.mpu = MPU(base_edge_addr=0x80000000)
-        self.mem_ctrl = MPUMemory(
-            vertex_range=AddrRange(start=0x000000, size="2GiB"),
-            vertex_binary="graphs/epinions/graph_binaries/vertices_0",
-            edge_range=AddrRange(start=0x80000000, size="2GiB"),
-            edge_binary="graphs/epinions/graph_binaries/edgelist_0")
-        self.interconnect = SystemXBar()
-
         self.ctrl.req_port = self.interconnect.cpu_side_ports
-        self.mpu.setReqPort(self.interconnect.cpu_side_ports)
-        self.mpu.setRespPort(self.interconnect.mem_side_ports)
 
-        self.mpu.setVertexMemPort(self.mem_ctrl.getVertexPort())
-        self.mpu.setEdgeMemPort(self.mem_ctrl.getEdgePort())
+        self.mem_ctrl = MPUMemory(num_mpus, 32, "2GiB", "2GiB", graph_path)
 
-system = SEGA()
-root = Root(full_system = False, system = system)
+        mpus = []
+        for i in range(num_mpus):
+            mpus.append(MPU(base_edge_addr=self.mem_ctrl.getEdgeBaseAddr(i)))
+            mpus[i].setReqPort(self.interconnect.cpu_side_ports)
+            mpus[i].setRespPort(self.interconnect.mem_side_ports)
+            mpus[i].setVertexMemPort(self.mem_ctrl.getVertexPort(i))
+            mpus[i].setEdgeMemPort(self.mem_ctrl.getEdgePort(i))
+        self.mpu = mpus
 
-m5.instantiate()
+def get_inputs():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("num_mpus", type=int)
+    argparser.add_argument("graph_path", type=str)
+    args = argparser.parse_args()
+    return args.num_mpus, args.graph_path
 
-exit_event = m5.simulate()
-print("Simulation finished!")
-exit()
+if __name__ == "__m5_main__":
+    num_mpus, graph_path = get_inputs()
+    print(f"Creating a system with {num_mpus} mpu(s) and graph {graph_path}")
+    system = SEGA(num_mpus, graph_path)
+    root = Root(full_system = False, system = system)
+
+    m5.instantiate()
+
+    exit_event = m5.simulate()
+    print("Simulation finished!")
+    exit()
