ed75380d9ea255d62deecd7d33b1e88735ca1e9c
Jasjeet Rangi <jasrangi@ucdavis.edu>
2022-08-10 10:21:42 -0700

    stdlib: Add board and config script for HiFive Unmatched
    Add board HiFiveUnmatchedBoard and run script. The run script can be
    given a binary to run with argument --riscv_binary and one optional
    command line argument to the binary with argument --argv.

---
+++ src/python/gem5/prebuilt/hifiveunmatched/hifive-run.py
@@ -0,0 +1,33 @@
+import argparse
+from gem5.resources.resource import Resource, CustomResource
+from gem5.simulate.simulator import Simulator
+from python.gem5.prebuilt.hifiveunmatched.hifive_board import HiFiveUnmatchedBoard
+from typing import List
+
+# collect optional CLI arg for RISCV binary to run
+parser = argparse.ArgumentParser(description="Binary to run on system")
+parser.add_argument(
+    "--riscv_binary", type=str, help="The RISCV binary to execute on the CPU",
+    default="riscv-hello",
+)
+parser.add_argument(
+    "--argv", type=str, help="CLI argument to the binary",
+    default=""
+)
+args = parser.parse_args()
+
+board = HiFiveUnmatchedBoard()
+
+# run default binary if no binary provided, otherwise run CLI provided resource
+if args.riscv_binary == "riscv-hello":
+    board.set_se_binary_workload(
+        Resource(args.riscv_binary)
+    )
+else:
+    board.set_se_binary_workload(
+        CustomResource(args.riscv_binary),
+        arguments=[args.argv]
+    )
+
+simulator = Simulator(board=board)
+simulator.run()
---
+++ src/python/gem5/prebuilt/hifiveunmatched/hifive_board.py
@@ -0,0 +1,30 @@
+from gem5.components.memory.dram_interfaces.ddr4 import DDR4_2400_8x8
+from gem5.components.memory.memory import ChanneledMemory
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.utils.requires import requires
+from gem5.isas import ISA
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.processors.cpu_types import CPUTypes, CustomCPUTypes
+from hifive_cache import HiFiveCacheHierarchy
+from m5.objects import AddrRange
+
+class HiFiveUnmatchedBoard(SimpleBoard):
+    def __init__(self) -> None:
+        requires(isa_required=ISA.RISCV)
+
+        cache_hierarchy = HiFiveCacheHierarchy(
+            l1d_size="32kB", l1i_size="32kB", l2_size="2MB"
+        )
+
+        memory = ChanneledMemory(DDR4_2400_8x8, 1, 64, "16GB")
+        memory.set_memory_range(
+            [AddrRange(start=0x80000000, size=memory.get_size())])
+
+        processor = SimpleProcessor(cpu_type=CustomCPUTypes.U74, num_cores=1)
+
+        super().__init__(
+            clk_freq="1.2GHz", # real system is 1.0 to 1.5 GHz
+            processor=processor,
+            memory=memory,
+            cache_hierarchy=cache_hierarchy,
+        )
