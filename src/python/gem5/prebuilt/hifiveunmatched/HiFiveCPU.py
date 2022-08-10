961768f72199266bb6c159511730983f943d843e
kunpai <kunpai@ucdavis.edu>
2022-08-10 10:05:40 -0700

    stdlib: Added components for a prebuilt RISC-V board
    Modelling a RISC-V board whose specs are mentioned in README.md
    Modeled a cache hierarchy and CPU based on the specs.

---
+++ src/python/gem5/prebuilt/hifiveunmatched/HiFiveCPU.py
@@ -0,0 +1,109 @@
+# Copyright (c) 2022 The Regents of the University of California.
+# All Rights Reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import m5
+from m5.objects.BaseMinorCPU import *
+from gem5.isas import ISA
+
+
+class U74IntFU(MinorDefaultIntFU):
+    opLat = 1
+
+
+class U74IntMulFU(MinorDefaultIntMulFU):
+    opLat = 3
+
+
+class U74IntDivFU(MinorDefaultIntDivFU):
+    opLat = 6  # not implemented 6 or 68 cycles
+    # microops or another method to implement division?
+    pass
+
+
+class U74FloatSimdFU(MinorDefaultFloatSimdFU):
+    pass
+
+
+class U74PredFU(MinorDefaultPredFU):
+    pass
+
+
+class U74MemFU(MinorDefaultMemFU):
+    opLat = 3
+
+
+class U74MiscFU(MinorDefaultMiscFU):
+    pass
+
+
+class U74FUPool(MinorFUPool):
+    funcUnits = [
+        U74IntFU(),
+        U74IntFU(),
+        U74IntMulFU(),
+        U74IntDivFU(),
+        U74FloatSimdFU(),
+        U74PredFU(),
+        U74MemFU(),
+        U74MiscFU(),
+    ]
+
+
+class U74BP(TournamentBP):
+    BTBEntries = 16
+    RASSize = 6
+    localHistoryTableSize = 4096  # is 3.6 KiB but gem5 requires power of 2
+
+    indirectBranchPred = SimpleIndirectPredictor()
+    indirectBranchPred.indirectSets = 8
+
+
+# create U74 from SimpleProcessor parent for stdlib compatibility
+class U74Processor(SimpleProcessor):
+    def __init__(
+        self,
+        cpu_type,
+        num_cores,
+    ) -> None:
+        super().__init__(
+            cpu_type=cpu_type,
+            num_cores=num_cores,
+            isa=ISA.RISCV,
+        )
+
+
+# RiscvCPU base class and RiscvMMU() are in src/arch/riscv/RiscvCPU.py
+class RiscvU74CPU(BaseMinorCPU, RiscvCPU):
+    fetch1FetchLimit = 2
+    fetch1ToFetch2BackwardDelay = 0
+    fetch2InputBufferSize = 1
+    decodeInputBufferSize = 1
+    executeInputBufferSize = 1
+    decodeToExecuteForwardDelay = 2
+    executeFuncUnits = U74FUPool()
+    branchPred = U74BP()
+    mmu = RiscvMMU()
