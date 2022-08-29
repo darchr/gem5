fbcc15b2ec956d233cfa0f4be1312a77e1913685
Mahyar Samani <msamani@ucdavis.edu>
2022-08-28 21:14:54 -0700

    Completed sim exit. I think...

--- configs/accl/sega.py
+++ configs/accl/sega.py
@@ -4,112 +4,8 @@ import argparse
 from math import log
 import math
 from m5.objects import *
-from m5.util.convert import toMemorySize
 
-class MPU(SubSystem):
-    def __init__(self):
-        super(MPU, self).__init__()
-        self.push_engine = PushEngine(push_req_queue_size=32,
-                                    attached_memory_atom_size=64,
-                                    resp_queue_size=64)
-        # self.push_engine = PushEngine(base_edge_addr=base_edge_addr,
-        #                             push_req_queue_size=32,
-        #                             attached_memory_atom_size=64,
-        #                             resp_queue_size=64)
-        self.coalesce_engine = CoalesceEngine(
-                                    peer_push_engine=self.push_engine,
-                                    attached_memory_atom_size=32,
-                                    cache_size="16MiB",
-                                    num_mshr_entry=32,
-                                    num_tgts_per_mshr=16)
-        self.wl_engine = WLEngine(coalesce_engine=self.coalesce_engine,
-                                update_queue_size=64,
-                                register_file_size=32)
-
-    def getRespPort(self):
-        return self.wl_engine.resp_port
-    def setRespPort(self, port):
-        self.wl_engine.resp_port = port
-
-    def getReqPort(self):
-        return self.push_engine.req_port
-    def setReqPort(self, port):
-        self.push_engine.req_port = port
-
-    def getVertexMemPort(self):
-        return self.coalesce_engine.mem_port
-    def setVertexMemPort(self, port):
-        self.coalesce_engine.mem_port = port
-
-    def getEdgeMemPort(self):
-        return self.push_engine.mem_port
-    def setEdgeMemPort(self, port):
-        self.push_engine.mem_port = port
-
-class MPUMemory(SubSystem):
-    def __init__(self,
-                    num_channels: int,
-                    cache_line_size: int,
-                    vertex_memory_size: str,
-                    edge_memory_size: str,
-                    graph_path: str):
-        super(MPUMemory, self).__init__()
-
-        self._vertex_ranges = self._interleave_addresses(
-                                AddrRange(start=0, size=vertex_memory_size),\
-                                num_channels,\
-                                cache_line_size)
-
-        self._edge_chunk_size = int(\
-                                toMemorySize(edge_memory_size)/num_channels)
-        self._edge_ranges = [AddrRange(
-                            start=toMemorySize(vertex_memory_size)+\
-                            self._edge_chunk_size*i,\
-                            size=self._edge_chunk_size)\
-                            for i in range(num_channels)]
-
-        vertex_mem_ctrl = []
-        edge_mem_ctrl = []
-        # vertex_mem_ranges = self._vertex_ranges
-        
-
-        for i in range(num_channels):
-            # vertex_addr_range = vertex_mem_ranges[i]
-            vertex_interface = DDR4_2400_8x8()
-            vertex_interface.range = self._vertex_ranges[i]
-            ctrl = MemCtrl()
-            ctrl.dram = vertex_interface
-            vertex_mem_ctrl.append(
-                ctrl
-            )
-
-            edge_interface = DDR4_2400_8x8(
-                image_file = f"{graph_path}/edgelist_{i}", in_addr_map=False)
-            edge_interface.range = AddrRange(self._edge_chunk_size)
-            #                 start=toMemorySize(vertex_memory_size)+\
-            #                 self._edge_chunk_size*i,\
-            #                 size=self._edge_chunk_size)
-            # edge_addr_range = edge_mem_range[0]
-            # edge_interface.range = self._edge_chunk_size
-            edge_ctrl = MemCtrl()
-            edge_ctrl.dram = edge_interface
-            edge_mem_ctrl.append(
-                edge_ctrl
-            )
-            # edge_mem_ctrl.append(
-            #     SimpleMemory(range=AddrRange(self._edge_chunk_size),
-            #                 bandwidth="4.8GB/s",
-            #                 latency="30ns",
-            #                 image_file=f"{graph_path}/edgelist_{i}",
-            #                 in_addr_map=False)
-            # )
-        self.vertex_mem_ctrl = vertex_mem_ctrl
-        self.edge_mem_ctrl = edge_mem_ctrl
-
-    def _interleave_addresses(self,
-                            plain_range,
-                            num_channels,
-                            cache_line_size):
+def interleave_addresses(plain_range, num_channels, cache_line_size):
         intlv_low_bit = log(cache_line_size, 2)
         intlv_bits = log(num_channels, 2)
         ret = []
@@ -123,17 +19,48 @@ class MPUMemory(SubSystem):
                 intlvMatch=i))
         return ret
 
-    def getVertexPort(self, i):
-        return self.vertex_mem_ctrl[i].port
-    def setVertexPort(self, port, i):
-        self.vertex_mem_ctrl[i].port = port
+class GPT(SubSystem):
+    def __init__(self, edge_memory_size: str):
+        super().__init__()
+        self.wl_engine = WLEngine(update_queue_size=64,
+                                register_file_size=32)
+        self.coalesce_engine = CoalesceEngine(attached_memory_atom_size=32,
+                                            cache_size="8MiB",
+                                            num_mshr_entry=32,
+                                            num_tgts_per_mshr=16)
+        self.push_engine = PushEngine(push_req_queue_size=32,
+                                    attached_memory_atom_size=64,
+                                    resp_queue_size=64)
+        self.vertex_mem_ctrl = SimpleMemory(latency="30ns",
+                                            latency_var="0ns",
+                                            bandwidth="19.2GiB/s")
+        self.edge_mem_ctrl = SimpleMemory(latency="30ns",
+                                        latency_var="0ns",
+                                        bandwidth="19.2GiB/s",
+                                        range=AddrRange(edge_memory_size),
+                                        in_addr_map=False)
 
-    def getEdgeBaseAddr(self, i):
-        return self._edge_ranges[i].start
-    def getEdgePort(self, i):
-        return self.edge_mem_ctrl[i].port
-    def setEdgePort(self, port, i):
-        self.edge_mem_ctrl[i].port = port
+        self.coalesce_engine.mem_port = self.vertex_mem_ctrl.port
+        self.push_engine.mem_port = self.edge_mem_ctrl.port
+
+        self.mpu = MPU(wl_engine=self.wl_engine,
+                    coalesce_engine=self.coalesce_engine,
+                    push_engine=self.push_engine)
+
+    def getRespPort(self):
+        return self.mpu.in_port
+    def setRespPort(self, port):
+        self.mpu.in_port = port
+
+    def getReqPort(self):
+        return self.mpu.out_port
+    def setReqPort(self, port):
+        self.mpu.out_port = port
+
+    def set_vertex_range(self, vertex_range):
+        self.vertex_mem_ctrl.range = vertex_range
+    def set_edge_image(self, edge_image):
+        self.edge_mem_ctrl.image_file = edge_image
 
 class SEGA(System):
     def __init__(self,
@@ -158,21 +85,19 @@ class SEGA(System):
                                     image_file=f"{graph_path}/vertices")
         self.ctrl.req_port = self.interconnect.cpu_side_ports
 
-        self.mem_ctrl = MPUMemory(
-                            num_mpus,
-                            self.cache_line_size,
-                            "2GiB",
-                            "14GiB",
-                            graph_path)
+        vertex_ranges = interleave_addresses(AddrRange("4GiB"), num_mpus, vertex_cache_line_size)
 
-        mpus = []
+        gpts = []
         for i in range(num_mpus):
-            mpus.append(MPU())
-            mpus[i].setReqPort(self.interconnect.cpu_side_ports)
-            mpus[i].setRespPort(self.interconnect.mem_side_ports)
-            mpus[i].setVertexMemPort(self.mem_ctrl.getVertexPort(i))
-            mpus[i].setEdgeMemPort(self.mem_ctrl.getEdgePort(i))
-        self.mpu = mpus
+            gpt = GPT("8GiB")
+            gpt.set_vertex_range(vertex_ranges[i])
+            gpt.set_edge_image(f"{graph_path}/edgelist_{i}")
+            gpt.setReqPort(self.interconnect.cpu_side_ports)
+            gpt.setRespPort(self.interconnect.mem_side_ports)
+            gpts.append(gpt)
+        self.gpts = gpts
+
+        self.ctrl.mpu_vector = [gpt.mpu for gpt in self.gpts]
 
 def get_inputs():
     argparser = argparse.ArgumentParser()
@@ -197,5 +122,4 @@ if __name__ == "__m5_main__":
     m5.instantiate()
 
     exit_event = m5.simulate()
-    print(f"Exited simulation because {exit_event.getCause()}")
-    exit()
+    print(f"Exited simulation at tick {m5.curTick()} because {exit_event.getCause()}")
