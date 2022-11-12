8f193ac46deeba7f71db55835ea69fe4ffc1bcec
Mahyar Samani <msamani@ucdavis.edu>
2022-11-12 13:54:47 -0800

    Debug.

--- src/accl/graph/base/graph_workload.cc
+++ src/accl/graph/base/graph_workload.cc
@@ -92,9 +92,9 @@ BFSWorkload::propagate(uint32_t value, uint32_t weight)
 }
 
 bool
-BFSWorkload::activeCondition(WorkListItem wl)
+BFSWorkload::activeCondition(WorkListItem new_wl, WorkListItem old_wl)
 {
-    return (wl.tempProp < wl.prop) && (wl.degree > 0);
+    return (new_wl.tempProp < old_wl.tempProp) && (old_wl.degree; > 0);
 }
 
 uint32_t
@@ -113,75 +113,75 @@ BFSWorkload::printWorkListItem(const WorkListItem wl)
             );
 }
 
-void
-PRWorkload::init(PacketPtr pkt, WorkDirectory* dir)
-{
-    size_t pkt_size = pkt->getSize();
-    int num_elements = (int) (pkt_size / sizeof(WorkListItem));
-    WorkListItem items[num_elements];
+// void
+// PRWorkload::init(PacketPtr pkt, WorkDirectory* dir)
+// {
+//     size_t pkt_size = pkt->getSize();
+//     int num_elements = (int) (pkt_size / sizeof(WorkListItem));
+//     WorkListItem items[num_elements];
 
-    pkt->writeDataToBlock((uint8_t*) items, pkt_size);
-    bool atom_active = false;
-    for (int i = 0; i < num_elements; i++) {
-        items[i].tempProp = readFromFloat<uint32_t>(0);
-        items[i].prop = readFromFloat<uint32_t>(1 - alpha);
-        atom_active |= activeCondition(items[i]);
-    }
-    if (atom_active) {
-        dir->activate(pkt->getAddr());
-    }
-    pkt->deleteData();
-    pkt->allocate();
-    pkt->setDataFromBlock((uint8_t*) items, pkt_size);
-}
+//     pkt->writeDataToBlock((uint8_t*) items, pkt_size);
+//     bool atom_active = false;
+//     for (int i = 0; i < num_elements; i++) {
+//         items[i].tempProp = readFromFloat<uint32_t>(0);
+//         items[i].prop = readFromFloat<uint32_t>(1 - alpha);
+//         atom_active |= activeCondition(items[i]);
+//     }
+//     if (atom_active) {
+//         dir->activate(pkt->getAddr());
+//     }
+//     pkt->deleteData();
+//     pkt->allocate();
+//     pkt->setDataFromBlock((uint8_t*) items, pkt_size);
+// }
 
-uint32_t
-PRWorkload::reduce(uint32_t update, uint32_t value)
-{
-    float update_float = writeToFloat<uint32_t>(update);
-    float value_float = writeToFloat<uint32_t>(value);
-    return readFromFloat<uint32_t>(update_float + value_float);
-}
+// uint32_t
+// PRWorkload::reduce(uint32_t update, uint32_t value)
+// {
+//     float update_float = writeToFloat<uint32_t>(update);
+//     float value_float = writeToFloat<uint32_t>(value);
+//     return readFromFloat<uint32_t>(update_float + value_float);
+// }
 
-uint32_t
-PRWorkload::propagate(uint32_t value, uint32_t weight)
-{
-    float value_float = writeToFloat<uint32_t>(value);
-    float weight_float = writeToFloat<uint32_t>(weight);
-    if (weight == 0) {
-        weight_float = 1.0;
-    }
-    return readFromFloat<uint32_t>(alpha * value_float * weight_float);
-}
+// uint32_t
+// PRWorkload::propagate(uint32_t value, uint32_t weight)
+// {
+//     float value_float = writeToFloat<uint32_t>(value);
+//     float weight_float = writeToFloat<uint32_t>(weight);
+//     if (weight == 0) {
+//         weight_float = 1.0;
+//     }
+//     return readFromFloat<uint32_t>(alpha * value_float * weight_float);
+// }
 
-bool
-PRWorkload::activeCondition(WorkListItem wl)
-{
-    float temp_float = writeToFloat<uint32_t>(wl.tempProp);
-    float prop_float = writeToFloat<uint32_t>(wl.prop);
-    float dist = std::abs(temp_float - prop_float);
-    return (dist >= threshold) && (wl.degree > 0);
-}
+// bool
+// PRWorkload::activeCondition(WorkListItem wl)
+// {
+//     float temp_float = writeToFloat<uint32_t>(wl.tempProp);
+//     float prop_float = writeToFloat<uint32_t>(wl.prop);
+//     float dist = std::abs(temp_float - prop_float);
+//     return (dist >= threshold) && (wl.degree > 0);
+// }
 
-uint32_t
-PRWorkload::apply(WorkListItem& wl)
-{
-    float temp_float = writeToFloat<uint32_t>(wl.tempProp);
-    float prop_float = writeToFloat<uint32_t>(wl.prop);
-    float delta = (temp_float - prop_float) / wl.degree;
-    uint32_t delta_uint = readFromFloat<uint32_t>(delta);
-    wl.prop = wl.tempProp;
-    return delta_uint;
-}
+// uint32_t
+// PRWorkload::apply(WorkListItem& wl)
+// {
+//     float temp_float = writeToFloat<uint32_t>(wl.tempProp);
+//     float prop_float = writeToFloat<uint32_t>(wl.prop);
+//     float delta = (temp_float - prop_float) / wl.degree;
+//     uint32_t delta_uint = readFromFloat<uint32_t>(delta);
+//     wl.prop = wl.tempProp;
+//     return delta_uint;
+// }
 
-std::string
-PRWorkload::printWorkListItem(const WorkListItem wl)
-{
-    float temp_float = writeToFloat<uint32_t>(wl.tempProp);
-    float prop_float = writeToFloat<uint32_t>(wl.prop);
-    return csprintf(
-            "WorkListItem{tempProp: %f, prop: %f, degree: %u, edgeIndex: %u}",
-            temp_float, prop_float, wl.degree, wl.edgeIndex);
-}
+// std::string
+// PRWorkload::printWorkListItem(const WorkListItem wl)
+// {
+//     float temp_float = writeToFloat<uint32_t>(wl.tempProp);
+//     float prop_float = writeToFloat<uint32_t>(wl.prop);
+//     return csprintf(
+//             "WorkListItem{tempProp: %f, prop: %f, degree: %u, edgeIndex: %u}",
+//             temp_float, prop_float, wl.degree, wl.edgeIndex);
+// }
 
 } // namespace gem5
