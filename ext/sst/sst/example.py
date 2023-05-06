8421007c506469149d612e023480d5da241b98d3
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-05-05 23:52:41 -0700

    ext: Update numa_sst configuration
    Change-Id: I2cf5a99f351cf1ef9c0e317daa7f1986312a6085
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- ext/sst/sst/example.py
+++ ext/sst/sst/example.py
@@ -30,7 +30,7 @@ import os
 
 from sst import UnitAlgebra
 
-cache_link_latency = "1ps"
+default_link_latency = "10ns"
 
 bbl = "riscv-boot-exit-nodisk"
 cpu_clock_rate = "3GHz"
@@ -41,38 +41,23 @@ memory_size_gem5 = "4GiB"
 memory_size_sst = "6GiB"
 addr_range_end = UnitAlgebra(memory_size_sst).getRoundedValue()
 
-l1_params = {
-    "access_latency_cycles" : "1",
-    "cache_frequency" : cpu_clock_rate,
-    "replacement_policy" : "lru",
-    "coherence_protocol" : "MESI",
-    "associativity" : "4",
-    "cache_line_size" : "64",
-    "cache_size" : "4 KiB",
-    "L1" : "1",
-}
-
 cpu_params = {
     "frequency": cpu_clock_rate,
-    "cmd": " ../../configs/example/sst/riscv_fs.py --cpu-clock-rate {} --memory-size {}".format(cpu_clock_rate, memory_size_gem5),
+    "cmd": " ../../disaggregated_memory_setup/numa_sst.py --command={}".format("numastat"),
     "debug_flags": ""
 }
 
 gem5_node = sst.Component("gem5_node", "gem5.gem5Component")
 gem5_node.addParams(cpu_params)
 
-cache_bus = sst.Component("cache_bus", "memHierarchy.Bus")
-cache_bus.addParams( { "bus_frequency" : cpu_clock_rate } )
+main_bus = sst.Component("main_bus", "memHierarchy.Bus")
+main_bus.addParams( { "bus_frequency" : cpu_clock_rate } )
 
 system_port = gem5_node.setSubComponent("system_port", "gem5.gem5Bridge", 0) # for initialization
-system_port.addParams({ "response_receiver_name": "system.system_outgoing_bridge"}) # tell the SubComponent the name of the corresponding SimObject
+system_port.addParams({ "response_receiver_name": "board.system_outgoing_bridge"}) # tell the SubComponent the name of the corresponding SimObject
 
 cache_port = gem5_node.setSubComponent("cache_port", "gem5.gem5Bridge", 0) # SST -> gem5
-cache_port.addParams({ "response_receiver_name": "system.memory_outgoing_bridge"})
-
-# L1 cache
-l1_cache = sst.Component("l1_cache", "memHierarchy.Cache")
-l1_cache.addParams(l1_params)
+cache_port.addParams({ "response_receiver_name": "board.remote_memory_outgoing_bridge"})
 
 # Memory
 memctrl = sst.Component("memory", "memHierarchy.MemController")
@@ -84,37 +69,32 @@ memctrl.addParams({
 })
 memory = memctrl.setSubComponent("backend", "memHierarchy.simpleMem")
 memory.addParams({
-    "access_time" : "30ns",
+    "access_time" : "15ns",
     "mem_size" : memory_size_sst
 })
 
 # Connections
-# cpu <-> L1
-cpu_cache_link = sst.Link("cpu_l1_cache_link")
-cpu_cache_link.connect(
-    (cache_port, "port", cache_link_latency),
-    (cache_bus, "high_network_0", cache_link_latency)
+
+node_bus_link = sst.Link("node_bus_link")
+node_bus_link.connect(
+    (cache_port, "port", default_link_latency),
+    (main_bus, "high_network_0", default_link_latency)
 )
-system_cache_link = sst.Link("system_cache_link")
-system_cache_link.connect(
-    (system_port, "port", cache_link_latency),
-    (cache_bus, "high_network_1", cache_link_latency)
+
+system_bus_link = sst.Link("system_bus_link")
+system_bus_link.connect(
+    (system_port, "port", default_link_latency),
+    (main_bus, "high_network_1", default_link_latency)
 )
-cache_bus_cache_link = sst.Link("cache_bus_cache_link")
-cache_bus_cache_link.connect(
-    (cache_bus, "low_network_0", cache_link_latency),
-    (l1_cache, "high_network_0", cache_link_latency)
-)
-# L1 <-> mem
-cache_mem_link = sst.Link("l1_cache_mem_link")
-cache_mem_link.connect(
-    (l1_cache, "low_network_0", cache_link_latency),
-    (memctrl, "direct_link", cache_link_latency)
+
+bus_mem_link = sst.Link("bus_mem_link")
+bus_mem_link.connect(
+    (main_bus, "low_network_0", default_link_latency),
+    (memctrl, "direct_link", default_link_latency)
 )
 
 # enable Statistics
 stat_params = { "rate" : "0ns" }
 sst.setStatisticLoadLevel(5)
 sst.setStatisticOutput("sst.statOutputTXT", {"filepath" : "./sst-stats.txt"})
-sst.enableAllStatisticsForComponentName("l1_cache", stat_params)
 sst.enableAllStatisticsForComponentName("memory", stat_params)
