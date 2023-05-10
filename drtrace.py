e550a4edbb4747c09c57448aa99768cf5aa9eac9
Ayaz Akram <yazakram@ucdavis.edu>
2023-05-09 20:32:48 -0700

    config: update one of the scripts

--- drtrace.py
+++ drtrace.py
@@ -1,6 +1,41 @@
 import m5
-
+import argparse
 from m5.objects import *
+
+"""
+Usage:
+------
+
+```
+./build/X86/gem5.opt \
+    drtrace.py \
+    --workload <benchmark_name> \
+    --players <Number of players to use>
+```
+"""
+
+parser = argparse.ArgumentParser(
+    description="A script to run google traces."
+)
+
+benchmark_choices = ["charlie", "delta", "merced", "whiskey"]
+
+parser.add_argument(
+    "--workload",
+    type=str,
+    required=True,
+    help="Input the benchmark program to execute.",
+    choices=benchmark_choices,
+)
+
+parser.add_argument(
+    "--players",
+    type=int,
+    required=True,
+    help="Input the number of players to use.",
+)
+
+args = parser.parse_args()
 
 system = System()
 
@@ -12,13 +47,13 @@ system.clk_domain.voltage_domain = VoltageDomain()
 system.mem_mode = "timing"  # Use timing accesses
 system.mem_ranges = [AddrRange("8GiB")]  # Create an address range
 system.mem_ctrl = MemCtrl()
-system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram = DDR4_2400_8x8()
 system.mem_ctrl.dram.range = system.mem_ranges[0]
 
-players = 16
+players = args.players
 
 system.reader = DRTraceReader(
-    directory="/projects/google-traces/delta/", num_players=players
+    directory="/projects/google-traces/{}/".format(args.workload), num_players=args.players
 )
 system.players = [
     DRTracePlayer(
@@ -51,5 +86,5 @@ root = Root(full_system=False, system=system)
 m5.instantiate()
 
 print("Beginning simulation!")
-exit_event = m5.simulate()
+exit_event = m5.simulate(50000000000)
 print(f"Exiting @ tick {m5.curTick()} because {exit_event.getCause()}")
