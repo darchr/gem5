2897c6f488f3cffec01e6e810cbc07213d82b440
Ayaz Akram <yazakram@ucdavis.edu>
2023-05-12 09:08:40 -0700

    misc: update script

--- drtrace.py
+++ drtrace.py
@@ -35,7 +35,20 @@ parser.add_argument(
     help="Input the number of players to use.",
 )
 
+parser.add_argument(
+    "--dram",
+    type = str,
+    help = "Memory device to use"
+)
+
 args = parser.parse_args()
+
+
+MemTypes = {
+    'ddr4_2400' : DDR4_2400_16x4,
+    'hbm_2000' : HBM_2000_4H_1x64,
+    'ddr5_8400' : DDR5_8400_4x8
+}
 
 system = System()
 
@@ -46,9 +59,16 @@ system.clk_domain.voltage_domain = VoltageDomain()
 # Set up the system
 system.mem_mode = "timing"  # Use timing accesses
 system.mem_ranges = [AddrRange("8GiB")]  # Create an address range
-system.mem_ctrl = MemCtrl()
-system.mem_ctrl.dram = DDR4_2400_8x8()
-system.mem_ctrl.dram.range = system.mem_ranges[0]
+
+if args.dram == 'hbm_2000':
+    system.mem_ctrl = HBMCtrl()
+    system.mem_ctrl.dram = MemTypes[args.dram](range=AddrRange(start = '0', end = '8GiB', masks = [1 << 6], intlvMatch = 0))
+    system.mem_ctrl.dram_2 = MemTypes[args.dram](range=AddrRange(start = '0', end = '8GiB', masks = [1 << 6], intlvMatch = 1))
+else:
+    system.mem_ctrl = MemCtrl()
+    system.mem_ctrl.dram = MemTypes[args.dram](range=system.mem_ranges[0])
+
+#system.mem_ctrl.dram.range = system.mem_ranges[0]
 
 players = args.players
 
@@ -86,5 +106,5 @@ root = Root(full_system=False, system=system)
 m5.instantiate()
 
 print("Beginning simulation!")
-exit_event = m5.simulate(50000000000)
+exit_event = m5.simulate(100000000000)
 print(f"Exiting @ tick {m5.curTick()} because {exit_event.getCause()}")
