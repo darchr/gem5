f0fd7b196fcca8b68cea2f87f2310ea9ab57d366
Erin Le <ejle@ucdavis.edu>
2023-08-25 22:57:35 +0000

    misc: Adding output files from running microbenchmarks again
    After changing the U74MemReadFU and U74MemWrite parameters to have 1 and 2 as their high and low values, the microbenchmarks were run again.
    The output files are under the folder matched_board_tests_paper/microbench_2.
    
    Change-Id: Ice98d8bc9364b0f9d099dc09fac0d161c7e6dea6

--- example/microbench_pb_run.sh
+++ example/microbench_pb_run.sh
@@ -5,12 +5,19 @@
 # SIZE = 16, or some other size of hadamard matrix, provided that you have a high/low parameter json
 # FILE = the high/low json, which has a list of a low and high value for each parameter. The number of parameters in the high/low json should be less than SIZE.
 
-FOLDOVER=1
-ROW=15
+# FOLDOVER=0
+# ROW=5
 SIZE=16
+MAX_IND=$(($SIZE - 1))
 FILE="nopipe_params.json"
 
 # Go through each microbenchmark
+
+for FOLDOVER in 0 1;
+do
+for ROW in $(seq 0 $MAX_IND)
+do
+
 for benchmark in riscv-cca riscv-cce riscv-cch riscv-cch-st riscv-cci riscv-ccm riscv-crd riscv-crf riscv-cs1 riscv-cs3 riscv-dp1d riscv-dp1f riscv-dpcvt riscv-dpt riscv-dptd riscv-ed1 riscv-ef riscv-ei riscv-em1 riscv-em5 riscv-m-dyn riscv-mc riscv-mcs riscv-md riscv-mi riscv-mim riscv-mim2 riscv-mip riscv-ml2-bw-ld riscv-ml2-bw-ldst riscv-ml2-bw-st riscv-stc riscv-stl2;
 do
 
@@ -19,32 +26,35 @@ echo "Now running: foldover = ${FOLDOVER}, PB matrix row = ${ROW}, PB matrix siz
 echo "Foldover = ${FOLDOVER}, so the row number in the foldover matrix is" $((($SIZE*$FOLDOVER)+$ROW))
 
 # Check if directory exists. If not, make the directory.
-if [ ! -d ./matched_board_tests_paper/microbench ]; then
-    mkdir ./matched_board_tests_paper/microbench
+if [ ! -d ./matched_board_tests_paper/microbench_2 ]; then
+    mkdir ./matched_board_tests_paper/microbench_2
 fi
 
-if [ ! -d ./matched_board_tests_paper/microbench/$benchmark ]; then
-    mkdir ./matched_board_tests_paper/microbench/$benchmark
+if [ ! -d ./matched_board_tests_paper/microbench_2/$benchmark ]; then
+    mkdir ./matched_board_tests_paper/microbench_2/$benchmark
 fi
 
-if [ ! -d ./matched_board_tests_paper/microbench/$benchmark/${FOLDOVER}_${ROW}_${SIZE} ]; then
-    mkdir ./matched_board_tests_paper/microbench/$benchmark/${FOLDOVER}_${ROW}_${SIZE}
+if [ ! -d ./matched_board_tests_paper/microbench_2/$benchmark/${FOLDOVER}_${ROW}_${SIZE} ]; then
+    mkdir ./matched_board_tests_paper/microbench_2/$benchmark/${FOLDOVER}_${ROW}_${SIZE}
 fi
 
 #generate configured json
-# python3 json_maker.py --foldover=${FOLDOVER} --pb_row=${ROW} --matrix_size=${SIZE}  --file=${FILE}  --nums="one"
+python3 json_maker.py --foldover=${FOLDOVER} --pb_row=${ROW} --matrix_size=${SIZE}  --file=${FILE}  --nums="one"
 
 # Allows you to do several runs
 # for i in {1..3}
 # do
     # Check if directory exists. If not, make the directory.
-    # if [ ! -d ./matched_board_tests_paper/microbench/$benchmark/${FOLDOVER}_${ROW}_${SIZE}/${i} ]; then
-    #     mkdir ./matched_board_tests_paper/microbench/$benchmark/${FOLDOVER}_${ROW}_${SIZE}/${i}
+    # if [ ! -d ./matched_board_tests_paper/microbench_2/$benchmark/${FOLDOVER}_${ROW}_${SIZE}/${i} ]; then
+    #     mkdir ./matched_board_tests_paper/microbench_2/$benchmark/${FOLDOVER}_${ROW}_${SIZE}/${i}
     # fi
 
     # run simulation. Redirect terminal output to the output/stats directory.
-    /scr/bees/pb-test-board/build/RISCV/gem5.opt --outdir=matched_board_tests_paper/microbench/${benchmark}/${FOLDOVER}_${ROW}_${SIZE}/${i} simple-config.py --benchmark=${benchmark} --foldover=$FOLDOVER --pb_row=$ROW --matrix_size=$SIZE  > ./matched_board_tests_paper/microbench/${benchmark}/${FOLDOVER}_${ROW}_${SIZE}/${i}/terminal.txt
+    /scr/bees/pb-test-board/build/RISCV/gem5.opt --outdir=matched_board_tests_paper/microbench_2/${benchmark}/${FOLDOVER}_${ROW}_${SIZE}/${i} simple-config.py --benchmark=${benchmark} --foldover=$FOLDOVER --pb_row=$ROW --matrix_size=$SIZE  > ./matched_board_tests_paper/microbench_2/${benchmark}/${FOLDOVER}_${ROW}_${SIZE}/${i}/terminal.txt &
+    echo "Simulation for foldover ${FOLDOVER} and row ${ROW} launched."
 
 # done  # i loop
 
 done    # benchmark loop
+done    # row loop
+done    # foldover loop
