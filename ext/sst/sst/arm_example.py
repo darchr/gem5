309a73fde7fd92b5a4422e015ea701e67d63cf60
Kaustav Goswami <kggoswami@ucdavis.edu>
2023-10-04 17:11:05 -0700

    ext: Updated the gem5 SST Bridge to use SST 13.0.0
    This change updates the gem5 SST Bridge to use SST 13.0.0. Changes
    are made to replace SimpleMem class to StandardMem class as
    SimpleMem will be deprecated in SST 14 and above. In addition, the
    translator.hh is updated to translate more types of gem5 packets.
    A new parameter `ports` was added on SST's side when invoking the
    gem5 component which does not require recompiling the gem5
    component whenever a new outgoing bridge is added in a gem5 config.
    
    Change-Id: I45f0013bc35d088df0aa5a71951422cabab4d7f7
    Signed-off-by: Kaustav Goswami <kggoswami@ucdavis.edu>

--- ext/sst/sst/arm_example.py
+++ ext/sst/sst/arm_example.py
@@ -35,6 +35,18 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 Arm Limited
+# All rights reserved.
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
 
 import sst
 import sys
