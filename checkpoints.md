b03cb1aa88399ca334369bf029f0abca406fd185
Kaustav Goswami <kggoswami@ucdavis.edu>
2024-03-05 04:10:15 +0000

    mem,configs,ext: updated the instructions and streamlined the configs
    Signed-off-by: Kaustav Goswami <kggoswami@ucdavis.edu>

--- checkpoints.md
+++ checkpoints.md
@@ -5,14 +5,14 @@ This readme contains the progress of checkpointing in gem5 + SST setup.
 ## Status of the bridge
 
 The bridge only works with timing mode.
-It is inherited from SimObject.
+It is inherited from AbstractMem
 
 ## Code
 
 ```sh
-git clone git@github.com:kaustav-goswami/gem5.git
+git clone git@github.com:darchr/gem5.git
 cd gem5
-git checkout kg/sst-checkpoints
+git checkout kg/dm-sst-checkpoints-ver2
 
 # build libgem5
 # build sst
@@ -20,35 +20,53 @@ git checkout kg/sst-checkpoints
 
 ## Approach
 
+### Gem5 only setup
+
+1. For taking and restoring checkpoints in gem5-only setup use the following
+   command:
+```sh
+build/ARM/gem5.opt disaggregated_memory/configs/arm-gem5-only-numa-checkpoints.py --take-ckpt=True --ckpt-file=<path/to/a/checkpoint/dir>
+```
+2. To restore a checkpoint in a gem5-only setup, use the following command:
+```sh
+build/ARM/gem5.opt disaggregated_memory/configs/arm-gem5-only-numa-checkpoints.py --take-ckpt=False --restore-ckpt=True --ckpt-file<path/to/a/checkpoint/dir>
+```
+
+### Gem5 + SST setup
+
 1. Currently, we take checkpoint using a gem5 only setup with a local and a remote memory.
 2. The address ranges have to be modified when restoring the checkpoint's physical memory.
-3. The `m5.cpt` file has to be modified to remove the non-existent SimObjects (this has to be corrected ASAP (see TODO section.))
-4. We manually read and write the physical memory file.
-
-## Taking a checkpoint
-
+3. Make sure that the CPU type is either KVM or ATOMIC. This keeps the request
+   and the data at the outgoing bridge.
 ```sh
-# inside the gem5 directory
-build/ALL/gem5.opt disaggregated_memory/configs/checkpoints/arm-gem5-take-ckpt.py
-# The checkpoint will be created in `gem5-ckpt` directory.
+build/ARM/gem5.opt disaggregated_memory/configs/arm-numa-checkpoints.py --cpu-clock-rate 3GHz --instance 0 --cpu-type atomic --local-memory-size 2GiB --remote-memory-addr-range 4294967296,6442450944 --remote-memory-latency 750 --take-ckpt=True --ckpt-file=<path/to/a/checkpoint/dir>
 ```
-
-## Restoring the checkpoint in SST
-
-1. First we need to modify the `m5.cpt` file.
-    Do not restore the final `physmem` so modify the following line from `nbr_of_stores=9` to `nbr_of_stores=8`
-2. You'd need to modify the SimObjects which does not exist in the restore system, if any.
-3. The final `physmem` file is manually traversed and loaded into the memory right now.
-4. Look at `unserialize()` method on how the restoring part is implemented.
-5. We are using the `initData` object to push the data into SST's backend memory.
-
+4. The physmem file 0 *usually* corresponds to the remote memory.
+5. In the `outgoing_request_bridge.cc` file, make sure that the physical memory
+   file is correctly read. In the `unserialize()` method, modify this line:
+```cpp
+std::string physical_memory_file = "board.physmem.store0.pmem";
+```
+6. Make sure that the delta to restore the checkpoint in the `ext/sst/gem5.cc`
+   is set correctly. It should be correctly set by default.
+```cpp
+     // Tick conversion
+     // The main logic for synchronize SST Tick and gem5 Tick is here.
+     // next_end_tick = current_cycle * timeConverter->getFactor()
+     if(flag == false) {
+         flag = true;
+         base_time = gem5::curTick();
+         std::cout << base_time << std::endl;
+     }
+     uint64_t next_end_tick = \
+         timeConverter->convertToCoreTime(current_cycle) + base_time;
+```
+7. We manually read and write the physical memory file. To debug this, print
+   the contents of `initData` in the `outgoing_request_bridge.cc`.
+8. To restore the previously created checkpoint from SST, use the following
+   command:
 ```sh
-# in the ext/sst directory
-bin/sst  --add-lib-path=./ sst/example_arm_dm_board_ckpt.py
-# the restore script is here
-# disaggregated_memory/configs/checkpoints/arm-gem5-restore-ckpt.py
+bin/
 ```
+9. The execution should resume normally.
 
-## TODO Items in the short time
-
-1. RemoteExternalMemory should inherit from `AbstractMemorySystem`.
---
+++ disaggregated_memory/configs/arm-numa-checkpoints.py
@@ -0,0 +1,235 @@
+# Copyright (c) 2023 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+This script shows an example of running a full system ARM Ubuntu boot
+simulation using the gem5 library. This simulation boots Ubuntu 20.04 using
+1 TIMING CPU cores and executes `STREAM`. The simulation ends when the
+startup is completed successfully.
+
+* This script has to be executed from SST
+"""
+
+import argparse
+import os
+import sys
+
+# all the source files are one directory above.
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
+
+from boards.arm_sst_board import ArmSstDMBoard
+from cachehierarchies.dm_caches_sst import (
+    ClassicPrivateL1PrivateL2SharedL3SstDMCache,
+)
+from memories.external_remote_memory import ExternalRemoteMemory
+from memories.remote_memory_outgoing_bridge import RemoteMemoryOutgoingBridge
+
+import m5
+from m5.objects import (
+    AddrRange,
+    Root,
+)
+
+from m5.objects.RealView import VExpress_GEM5_V1
+
+from gem5.components.memory import (
+    DualChannelDDR4_2400,
+    SingleChannelDDR4_2400,
+)
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.isas import ISA
+from gem5.resources.resource import *
+from gem5.resources.workload import *
+from gem5.resources.workload import Workload
+from gem5.simulate.simulator import Simulator
+from gem5.utils.requires import requires
+
+# SST passes a couple of arguments for this system to simulate.
+parser = argparse.ArgumentParser()
+parser.add_argument("--command", type=str, help="Command run by guest")
+parser.add_argument(
+    "--cpu-type",
+    type=str,
+    choices=["atomic", "timing", "o3", "kvm"],
+    default="atomic",
+    help="CPU type",
+)
+parser.add_argument(
+    "--cpu-clock-rate",
+    type=str,
+    required=True,
+    help="CPU Clock",
+)
+parser.add_argument(
+    "--local-memory-size",
+    type=str,
+    required=True,
+    help="Local memory size",
+)
+parser.add_argument(
+    "--remote-memory-addr-range",
+    type=str,
+    required=True,
+    help="Remote memory range",
+)
+parser.add_argument(
+    "--remote-memory-latency",
+    type=int,
+    required=True,
+    help="Remote memory latency in Ticks (has to be converted prior)",
+)
+parser.add_argument(
+    "--instance",
+    type=int,
+    required=True,
+    help="Instance id is need to correctly read and write to the checkpoint."
+)
+
+parser.add_argument(
+    "--ckpt-file",
+    type=str,
+    default="",
+    required=False,
+    help="optionally put a path to restore a checkpoint"
+)
+parser.add_argument(
+    "--take-ckpt",
+    type=str,
+    default="False",
+    required=True,
+    help="optionally put a path to restore a checkpoint"
+)
+
+args = parser.parse_args()
+cpu_type = {
+    "o3": CPUTypes.O3,
+    "atomic": CPUTypes.ATOMIC,
+    "timing": CPUTypes.TIMING,
+    "kvm": CPUTypes.KVM
+}[args.cpu_type]
+
+remote_memory_range = list(map(int, args.remote_memory_addr_range.split(",")))
+remote_memory_range = AddrRange(remote_memory_range[0], remote_memory_range[1])
+
+# This runs a check to ensure the gem5 binary is compiled for ARM.
+requires(isa_required=ISA.ARM)
+# Here we setup the parameters of the l1 and l2 caches.
+cache_hierarchy = ClassicPrivateL1PrivateL2SharedL3SstDMCache(
+    l1d_size="32KiB", l1i_size="32KiB", l2_size="256KiB", l3_size="1MiB"
+)
+# Memory: Dual Channel DDR4 2400 DRAM device.
+
+local_memory = SingleChannelDDR4_2400(size=args.local_memory_size)
+
+# Either suppy the size of the remote memory or the address range of the
+# remote memory. Since this is inside the external memory, it does not matter
+# what type of memory is being simulated. This can either be initialized with
+# a size or a memory address range, which is mroe flexible. Adding remote
+# memory latency automatically adds a non-coherent crossbar to simulate latenyc
+
+remote_memory = ExternalRemoteMemory(
+    addr_range=remote_memory_range, link_latency=args.remote_memory_latency
+)
+
+# Here we setup the processor. We use a simple processor.
+processor = SimpleProcessor(cpu_type=cpu_type, isa=ISA.ARM, num_cores=1)
+
+# Here we setup the board which allows us to do Full-System ARM simulations.
+board = ArmSstDMBoard(
+    clk_freq=args.cpu_clock_rate,
+    processor=processor,
+    local_memory=local_memory,
+    remote_memory=remote_memory,
+    cache_hierarchy=cache_hierarchy,
+    platform=VExpress_GEM5_V1()
+)
+
+cmd = [
+    "mount -t sysfs - /sys;",
+    "mount -t proc - /proc;",
+    "numastat;",
+    "m5 exit;",  # checkpoint
+    "ls;",
+    "numactl --membind=0 -- ls;",  # +
+    # "/home/ubuntu/simple-vectorizable-microbenchmarks/stream-annotated/" +
+    # "stream.hw.m5 1000000;",
+    # "numastat;",
+    "numactl --interleave=0,1 -- " +
+    "/home/ubuntu/simple-vectorizable-microbenchmarks/stream-annotated/" +
+    "stream.hw.m5 1000000;",
+    "numastat;",
+    # "numactl --membind=1 -- " +
+    # "/home/ubuntu/simple-vectorizable-microbenchmarks/stream-annotated/" +
+    # "stream.hw.m5 1000000;",
+    # "numastat;",
+    "m5 exit;",
+]
+
+workload = CustomWorkload(
+    function="set_kernel_disk_workload",
+    parameters={
+        "kernel": CustomResource("/home/kaustavg/vmlinux-5.4.49-NUMA.arm64"),
+        "bootloader": CustomResource(
+            "/home/kaustavg/.cache/gem5/arm64-bootloader"
+        ),
+        "disk_image": DiskImageResource(
+            "/home/kaustavg/disk-images/arm/arm64sve-hpc-2204-20230526-numa.img",
+            # local_path="/projects/gem5/hn/DISK_IMAGES/arm64sve-hpc-2204-20230526-numa.img",
+            root_partition="1",
+        ),
+        "readfile_contents": " ".join(cmd),
+    },
+)
+ckpt_to_read_write = ""
+if args.ckpt_file != "":
+    ckpt_to_read_write = args.ckpt_file + str(args.instance)
+
+# This disk image needs to have NUMA tools installed.
+board.set_workload(workload)
+# This script will boot two numa nodes in a full system simulation where the
+# gem5 node will be sending instructions to the SST node. the simulation will
+# after displaying numastat information on the terminal, whjic can be viewed
+# from board.terminal.
+board._pre_instantiate()
+root = Root(full_system=True, board=board)
+board._post_instantiate()
+
+# restore checkpoint if any
+if args.take_ckpt == "True":
+    if args.cpu_type == "kvm":
+        root.sim_quantum = int(1e9)
+    m5.instantiate()
+    # probably this script is being called only in gem5
+    m5.simulate()
+    if ckpt_to_read_write != "":
+        m5.checkpoint(os.path.join(os.getcwd(), ckpt_to_read_write))
+else:
+    # This is called in SST. SST will take care of running this script.
+    m5.instantiate(ckpt_to_read_write)
+
