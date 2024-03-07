a1688bddbef91e072c88175287c451a2c45613a5
Kaustav Goswami <kggoswami@ucdavis.edu>
2024-03-07 22:24:14 +0000

    qol: added a script to build SST (core and elements) automatically
    Signed-off-by: Kaustav Goswami <kggoswami@ucdavis.edu>

---
+++ ext/sst/SST_INSTALL.sh
@@ -0,0 +1,22 @@
+# run this sceript in ext/sst
+# Build gem5 source before
+export SST_CORE_HOME=`pwd`
+wget https://github.com/sstsimulator/sst-core/releases/download/v13.0.0_Final/sstcore-13.0.0.tar.gz
+tar xf sstcore-13.0.0.tar.gz
+cd sstcore-13.0.0
+./configure --prefix=$SST_CORE_HOME --with-python=/usr/bin/python3-config
+make all -j32
+make install
+cd ..
+wget https://github.com/sstsimulator/sst-elements/releases/download/v13.0.0_Final/sstelements-13.0.0.tar.gz
+tar xf sstelements-13.0.0.tar.gz
+cd sst-elements-library-13.0.0
+./configure --prefix=$SST_CORE_HOME --with-python=/usr/bin/python3-config \
+                                        --with-sst-core=$SST_CORE_HOME
+make all -j32
+make install
+cd ..
+export PKG_CONFIG_PATH=`pwd`/lib/pkgconfig
+# build ALL/libgem5
+mv Makefile.linux Makefile
+make clean ; make ARCH=ALL -j4
