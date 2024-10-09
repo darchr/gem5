e29fd868bd66320c472e359013f199d37ec39c55
Jason Lowe-Power <jason@lowepower.com>
2024-10-08 17:45:13 -0700

    configs: Updates to x86 and cluster
    This allows parallel simulation for the x86 remote memory
    system. Arm isn't tested with parallel (or at all).
    
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- disaggregated-board/run_x86_cluster.py
+++ disaggregated-board/run_x86_cluster.py
@@ -22,50 +22,39 @@ from gem5.resources.resource import (
 from gem5.simulate.exit_event import ExitEvent
 from gem5.simulate.simulator import Simulator
 
-board1 = RemoteMemoryX86Board(
-    clk_freq="3GHz",
-    processor=SimpleProcessor(
-        cpu_type=CPUTypes.ATOMIC,
-        num_cores=1,
-        isa=ISA.X86,
-    ),
-    cache_hierarchy=PrivateL1PrivateL2CacheHierarchy(
-        l1d_size="32kB",
-        l1i_size="32kB",
-        l2_size="256kB",
-    ),
-    memory=SingleChannelDDR3_1600(size="2GiB"),
-)
+import argparse
 
-board2 = RemoteMemoryX86Board(
-    clk_freq="3GHz",
-    processor=SimpleProcessor(
-        cpu_type=CPUTypes.ATOMIC,
-        num_cores=1,
-        isa=ISA.X86,
-    ),
-    cache_hierarchy=PrivateL1PrivateL2CacheHierarchy(
-        l1d_size="32kB",
-        l1i_size="32kB",
-        l2_size="256kB",
-    ),
-    memory=SingleChannelDDR3_1600(size="2GiB"),
-)
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--num-boards", type=int, default=2)
+arg_parser.add_argument("--parallel", action="store_true", default=False)
+args = arg_parser.parse_args()
 
-# board1.set_se_binary_workload(binary=obtain_resource("x86-hello64-static"))
-# board2.set_se_binary_workload(binary=obtain_resource("x86-hello64-static"))
-
-# board1.set_workload(obtain_resource("x86-ubuntu-24.04-boot-no-systemd"))
-# board2.set_workload(obtain_resource("x86-ubuntu-24.04-boot-no-systemd"))
+def get_board():
+    board = RemoteMemoryX86Board(
+        clk_freq="3GHz",
+        processor=SimpleProcessor(
+            cpu_type=CPUTypes.TIMING,
+            num_cores=1,
+            isa=ISA.X86,
+        ),
+        cache_hierarchy=PrivateL1PrivateL2CacheHierarchy(
+            l1d_size="32KiB",
+            l1i_size="32KiB",
+            l2_size="256KiB",
+        ),
+        memory=SingleChannelDDR3_1600(size="2GiB"),
+    )
+    return board
 
 # Board 1 will write "Hello world!" and read it back
-command1 = "echo '12345' | sudo -S ./mount.sh; ./test-read-write"
+command1 = "echo '12345' | sudo -S ./mount.sh; sleep 1; gem5-bridge dumpresetstats; ./test-read-write"
 
-# Board 2 will wait for 1 second, then read what was written (hopefully, "hello world")
-command2 = "echo '12345' | sudo -S ./mount.sh; sleep 1; ./test-read"
+# other boards will wait for 1 second, then read what was written (hopefully, "hello world")
+command2 = "echo '12345' | sudo -S ./mount.sh; sleep 2; gem5-bridge dumpresetstats; ./test-read"
 
-board1.set_kernel_disk_workload(
-    # kernel=obtain_resource("x86-linux-kernel-6.8.0-35-generic"),
+boards = [get_board() for _ in range(args.boards)]
+
+boards[0].set_kernel_disk_workload(
     kernel=KernelResource("/home/jlp/Code/linux/vmlinux.x86"),
     disk_image=DiskImageResource(
         "/home/jlp/Code/gem5/gem5-resources/src/add-dax/disk-image/x86-ubuntu-24-04-dax"
@@ -78,48 +67,47 @@ board1.set_kernel_disk_workload(
     ],
     readfile_contents=command1,
 )
-board2.set_kernel_disk_workload(
-    # kernel=obtain_resource("x86-linux-kernel-6.8.0-35-generic"),
-    kernel=KernelResource("/home/jlp/Code/linux/vmlinux.x86"),
-    disk_image=DiskImageResource(
-        "/home/jlp/Code/gem5/gem5-resources/src/add-dax/disk-image/x86-ubuntu-24-04-dax"
-    ),
-    kernel_args=[
-        "earlyprintk=ttyS0",
-        "console=ttyS0",
-        "lpj=7999923",
-        "root=/dev/sda2",
-    ],
-    readfile_contents=command2,
-)
 
-board1.append_kernel_arg("memmap=128M!2G")
-board1.append_kernel_arg("no_systemd=true")
-board2.append_kernel_arg("memmap=128M!2G")
-board2.append_kernel_arg("no_systemd=true")
+for board in boards[1:]:
+    board.set_kernel_disk_workload(
+        kernel=KernelResource("/home/jlp/Code/linux/vmlinux.x86"),
+        disk_image=DiskImageResource(
+            "/home/jlp/Code/gem5/gem5-resources/src/add-dax/disk-image/x86-ubuntu-24-04-dax"
+        ),
+        kernel_args=[
+            "earlyprintk=ttyS0",
+            "console=ttyS0",
+            "lpj=7999923",
+            "root=/dev/sda2",
+        ],
+        readfile_contents=command2,
+    )
+
+for board in boards:
+    board.append_kernel_arg("memmap=128M!2G")
+    board.append_kernel_arg("no_systemd=true")
 
 cluster = Cluster(
-    boards=[board1, board2],
+    boards=boards,
     remote_memory=RemoteMemory(size="1GiB", start="2GiB"),
+    parallel=args.parallel,
 )
 
 
 def on_exit():
-    print("done booting kernel board 1")
-    yield False
-    print("done booting kernel board 2")
-    yield False
+    for i in range(len(boards)):
+        print(f"Exited for 'before boot' exit in board {i}")
+        yield False
 
-    print("Exited for 'after boot' exit in board 1")
-    yield False
-    print("Exited for 'after boot' exit in board 2")
-    yield False
+    for i in range(len(boards)):
+        print(f"Exited for 'after boot' exit in board {i}")
+        yield False
 
-    print("Finished simulation board 1")
-    yield False
-    print("Finished simulation board 2")
+    for i in range(len(boards) - 1):
+        print(f"finished board {i}")
+        yield False
+
     yield True
-
 
 simulator = Simulator(
     board=cluster,
