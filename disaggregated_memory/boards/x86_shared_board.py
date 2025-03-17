8b336ceeb1ba23fe816837b4ae26c6cea3000c63
Kaustav Goswami <kggoswami@ucdavis.edu>
2025-03-17 12:30:58 -0700

    stdlib,ext: added a common interface between SST and gem5
    This change adds a simplified common interface between gem5 and SST
    simulations that use checkpoints. Earlier a user needed to run gem5
    sumulations separately, copied over the checkpoints to SST's output
    directory and then start the SST simulation. There were two difference
    scripts for gem5 and SST. In this change, a high-level python interface
    is created, which combines gem5 and SST scripts together and users
    define a standard system which is common across both the simulators.
    Simply put, the entire simulation works with just one command.
    
    Signed-off-by: Kaustav Goswami <kggoswami@ucdavis.edu>

--- disaggregated_memory/boards/x86_shared_board.py
+++ disaggregated_memory/boards/x86_shared_board.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2023-24 The Regents of the University of California
 # All rights reserved.
 #
@@ -27,12 +28,21 @@
 # Creating an x86 board that can simulate more than 3 GB memory.
 
 import os
+import sys
 from abc import ABCMeta
 from typing import (
     List,
     Sequence,
     Tuple,
 )
+
+# all the source files are one directory above.
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
+
+# from memories.external_remote_memory import ExternalRemoteMemory
+from boards.x86_main_board import X86ComposableMemoryBoard
 
 import m5
 from m5.objects import (
@@ -44,7 +54,6 @@ from m5.objects import (
     CowDiskImage,
     IdeDisk,
     IOXBar,
-    NoncoherentXBar,
     OutgoingRequestBridge,
     Pc,
     Port,
@@ -53,13 +62,14 @@ from m5.objects import (
     Terminal,
     VncServer,
     VoltageDomain,
-    X86ACPIMadt,
-    X86ACPIMadtIntSourceOverride,
     X86E820Entry,
+    X86ACPIMadt,
+    NoncoherentXBar,
     X86IntelMPBus,
     X86IntelMPBusHierarchy,
     X86IntelMPIOAPIC,
     X86IntelMPIOIntAssignment,
+    X86ACPIMadtIntSourceOverride,
     X86IntelMPProcessor,
     X86SMBiosBiosInformation,
 )
@@ -73,45 +83,11 @@ from gem5.components.memory.abstract_memory_system import AbstractMemorySystem
 from gem5.components.processors.abstract_processor import AbstractProcessor
 from gem5.utils.override import overrides
 
-
-class X86ComposableMemoryBoard(X86Board):
+class X86SharedMemoryBoard(X86ComposableMemoryBoard):
     """
-    A high-level X86 board that can zNUMA-capable systems with a remote
-    memories. This board is extended from the ArmBoard from Gem5 standard
-    library. This board assumes that you will be booting Linux. This board can
-    be used to do disaggregated ARM system research while accelerating the
-    simulation using kvm.
-
-    The revised X86ComposableMemoryBoard combines the older boards into one
-    single board to make the boards compatible with both gem5 and SST.
-
-    Targets:
-        - This board should support memory hotplugging via PROBE
-        - We also need to get ACPI SRAT tables set up for the NUMA ranges.
-
-    Limitations:
-        - Local memory cannot be more than 3 GB (lazy to make this work).
-        - NUMA nodes are faked via the kernel as gem5 X86 does not support
-          ACPI SRAT tables.
-
-    Args:
-        :clk_freq:
-        :processor:
-        :local_memory:
-        :remote_memory:
-        :cache_hierarchy:
-        :remote_memory_access_cycles:
-        :remote_memory_address_range:
-        :starting_memory_limit:
-
-    Raises:
-        NotImplementedError: _description_
-        Exception: _description_
-
+    A high-level X86 board that allows users to simulate reserved memory
+    ranges to enable memory sharing.
     """
-
-    __metaclass__ = ABCMeta
-
     def __init__(
         self,
         clk_freq: str,
@@ -122,140 +98,21 @@ class X86ComposableMemoryBoard(X86Board):
         remote_memory_access_cycles: int = 0,
         remote_memory_address_range: AddrRange = None,
         starting_memory_limit: str = None,
+        
     ) -> None:
-        # The parent board calls get_memory(), which needs overriding.
-        self._localMemory = local_memory
-        self._remoteMemory = remote_memory
-        # We need to set the remote memory range before init for the remote
-        # memory. If the user did not specify the remote_memory_addr_range,
-        # then we'd assume that the remote memory starts where local memory
-        # ends.
-        if isinstance(remote_memory, OutgoingRequestBridge) == False:
-            if remote_memory_address_range is None:
-                # If the remote_memory_addr_range is not provided, we'll assume
-                # that it starts at 0x100000000 + local_memory_size and ends at
-                # it's own size
-                self._remoteMemoryAddressRange = AddrRange(
-                    0x100000000 + self._localMemory.get_size(),
-                    size=self._remoteMemory.get_size(),
-                )
-            else:
-                self._remoteMemoryAddressRange = remote_memory_address_range
-        else:
-            self._remoteMemoryAddressRange = None
+        # call the composable memory board that start the simulation.
         super().__init__(
             clk_freq=clk_freq,
             processor=processor,
-            memory=local_memory,
+            local_memory=local_memory,
+            remote_memory=remote_memory,
             cache_hierarchy=cache_hierarchy,
+            remote_memory_access_cycles=remote_memory_access_cycles,
+            remote_memory_address_range=remote_memory_address_range,
+            starting_memory_limit=starting_memory_limit
         )
 
-        self.local_memory = local_memory
-        self.remote_memory = remote_memory
-
-        self._remote_memory_access_cycles = remote_memory_access_cycles
-
-        # Set the external simulator variable to whatever the user has set in
-        # the ExternalRemoteMemory component.
-        self._external_simulator = False
-        if isinstance(self.get_remote_memory(), OutgoingRequestBridge):
-            # TODO: This needs to be standardized.
-            self._external_simulator = (
-                self.get_remote_memory()._remote_request_bridge.use_sst_sim
-            )
-
-    @overrides(X86Board)
-    def get_memory(self) -> AbstractMemorySystem:
-        """Get the memory (RAM) connected to the board.
-
-        :returns: The memory system.
-        """
-        raise NotImplementedError
-
-    def get_local_memory(self) -> AbstractMemorySystem:
-        """Get the memory (RAM) connected to the board.
-        :returns: The local memory system.
-        """
-        return self._localMemory
-
-    def get_remote_memory(self) -> AbstractMemorySystem:
-        """Get the memory (RAM) connected to the board.
-        :returns: The remote memory system.
-        """
-        return self._remoteMemory
-
-    def get_remote_memory_size(self) -> "str":
-        """Get the remote memory size to setup the NUMA nodes. Since the remote
-            memory is an abstract memory system, we should be able to call its
-            standard methods.
-        :returns: The size of the remote memory system.
-        """
-        return self.get_remote_memory().get_size()
-
-    @overrides(X86Board)
-    def get_mem_ports(self) -> Sequence[Tuple[AddrRange, Port]]:
-        return self.get_local_memory().get_mem_ports()
-
-    def get_remote_mem_ports(self) -> Sequence[Tuple[AddrRange, Port]]:
-        """Get the memory (RAM) ports connected to the board.
-            This has to be implemeted by the child class as we don't know if
-            this board is simulating Gem5 memory or some external simulator
-            memory.
-        :returns: A tuple of mem_ports.
-        """
-        return self.get_remote_memory().get_mem_ports()
-
-    def get_remote_memory_addr_range(self):
-        """Get the range of the remote memory. This can be omitted in the
-            future iteration of the board.
-        :returns: AddrRange of the remote memory
-        """
-        # Although this is hardcoded to return the first element, this is
-        # always valid. This is how the standard library returns
-        # get_mem_ports().
-        if self._remoteMemoryAddressRange is None:
-            return self.get_remote_mem_ports()[0][0]
-        else:
-            return self._remoteMemoryAddressRange
-
-    @overrides(X86Board)
-    def _setup_memory_ranges(self):
-        # Need to create 2 entries for the memory ranges
-        local_memory = self.get_local_memory()
-        remote_memory = self.get_remote_memory()
-
-        memory_size = [local_memory.get_size(), remote_memory.get_size()]
-
-        memory_ranges = [
-            AddrRange(start=0x0, size=local_memory.get_size()),
-            AddrRange(start=0x100000000, size=remote_memory.get_size()),
-        ]
-
-        self.mem_ranges = [
-            AddrRange(start=0x0, size=local_memory.get_size()),
-            AddrRange(start=0x100000000, size=remote_memory.get_size()),
-            AddrRange(0xC0000000, size=0x100000),  # For I/0
-        ]
-
-        local_memory.set_memory_range(
-            [AddrRange(start=0x0, size=local_memory.get_size())]
-        )
-        remote_memory.set_memory_range(
-            [AddrRange(start=0x100000000, size=remote_memory.get_size())]
-        )
-
-    @overrides(X86Board)
-    def get_default_kernel_args(self) -> List[str]:
-        return [
-            "earlyprintk=ttyS0",
-            "console=ttyS0",
-            "lpj=7999923",
-            "root=/dev/sda1",
-            # "init=/bin/bash",
-            "numa=fake=2",
-        ]
-
-    @overrides(X86Board)
+    @overrides(X86ComposableMemoryBoard)
     def _setup_io_devices(self):
         """Sets up the x86 IO devices.
 
@@ -423,10 +280,12 @@ class X86ComposableMemoryBoard(X86Board):
                 size=f"{self.mem_ranges[0].size() - 0x100000:d}B",
                 range_type=1,
             ),
+            # The second range of memory will be shared and the type of this
+            # range has to be 12 - `persistent`
             X86E820Entry(
                 addr=0x100000000,
                 size=f"{self.mem_ranges[1].size()}B",
-                range_type=1,
+                range_type=12,
             ),
         ]
 
@@ -434,92 +293,18 @@ class X86ComposableMemoryBoard(X86Board):
         entries.append(
             X86E820Entry(addr=0xFFFF0000, size="64kB", range_type=2)
         )
+        self.workload.e820_table.entries = entries        
 
-        print(entries)
-        self.workload.e820_table.entries = entries
 
-    def add_remote_link(self) -> None:
-        """This method creates a non-coherent xbar"""
-        self.remote_link = NoncoherentXBar(
-            frontend_latency=self._remote_memory_access_cycles,
-            forward_latency=0,
-            response_latency=0,
-            width=64,
-        )
-        # Connect the remote memory port to the remote link.
-        for _, port in self.get_remote_memory().get_mem_ports():
-            self.remote_link.mem_side_ports = port
-
-        # Connect the cpu side ports to the cache
-        self.remote_link.cpu_side_ports = (
-            self.get_cache_hierarchy().get_mem_side_port()
-        )
-
-    @overrides(AbstractBoard)
-    def _connect_things(self) -> None:
-        """Connects all the components to the board.
-
-        The order of this board is always:
-
-        1. Connect the memory.
-        2. Connect the cache hierarchy.
-        3. Connect the processor.
-
-        Developers may build upon this assumption when creating components.
-
-        Notes
-        -----
-
-        * The processor is incorporated after the cache hierarchy due to a bug
-        noted here: https://gem5.atlassian.net/browse/GEM5-1113. Until this
-        bug is fixed, this ordering must be maintained.
-        * Once this function is called `_connect_things_called` *must* be set
-        to `True`.
-        """
-
-        if self._connect_things_called:
-            raise Exception(
-                "The `_connect_things` function has already been called."
-            )
-
-        # Incorporate the memory into the motherboard.
-        self.get_local_memory().incorporate_memory(self)
-        self.get_remote_memory().incorporate_memory(self)
-
-        # Incorporate the cache hierarchy for the motherboard.
-        if self.get_cache_hierarchy():
-            self.get_cache_hierarchy().incorporate_cache(self)
-
-        # Create and connect Xbar for additional latency. This will override
-        # the cache's incorporate_cache
-        if (
-            self._remote_memory_access_cycles > 0
-            and self._external_simulator == False
-        ):
-            self.add_remote_link()
-        else:
-            # connect the system to the remote memory directly.
-            for cntr in self.get_remote_memory().get_memory_controllers():
-                cntr.port = self.get_cache_hierarchy().get_mem_side_port()
-        # Incorporate the processor into the motherboard.
-        self.get_processor().incorporate_processor(self)
-
-        self._connect_things_called = True
-
-    @overrides(AbstractBoard)
-    def _post_instantiate(self):
-        """Called to set up anything needed after m5.instantiate"""
-        self.get_processor()._post_instantiate()
-        if self.get_cache_hierarchy():
-            self.get_cache_hierarchy()._post_instantiate()
-        self.get_local_memory()._post_instantiate()
-        self.get_remote_memory()._post_instantiate()
-
-    @overrides(X86Board)
+    @overrides(X86ComposableMemoryBoard)
     def get_default_kernel_args(self) -> List[str]:
         return [
+            "no_systemd=true",
             "earlyprintk=ttyS0",
             "console=ttyS0",
             "lpj=7999923",
             "root=/dev/sda2",
+            # "mtrr=debug"
+            "no_systemd=true",
+            # "init=/bin/bash"
         ]
